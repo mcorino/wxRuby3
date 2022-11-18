4afe6a979dc733c83cbe9940e40fcc4ea795a8bf
Martin Corino <mcorino@m2c-software.nl>
2022-11-18 16:30:29 +0100

    adding ClipBoard support

--- rake/lib/director/data_object.rb
+++ rake/lib/director/data_object.rb
@@ -15,33 +15,45 @@ module WXRuby3
 
       def setup
         super
-        spec.items.concat %w[wxDataObjectSimple wxBitmapDataObject]
+        spec.items.concat %w[wxDataObjectSimple wxBitmapDataObject wxFileDataObject wxTextDataObject wxCustomDataObject wxImageDataObject wxURLDataObject]
         spec.gc_as_object
         spec.swig_include '../shared/data_format.i'
         spec.swig_include '../shared/data_object_common.i'
+
+        # we only allow Ruby derivatives from wxDataObject but not of any of the C++ implemented
+        # specializations
+        %w[wxDataObjectSimple wxBitmapDataObject wxFileDataObject wxTextDataObject wxCustomDataObject wxImageDataObject wxURLDataObject].each do |kls|
+          spec.no_proxy kls
+        end
+
         # ignore the shortened 'convenience' forms
         spec.ignore 'wxDataObjectSimple::GetDataHere(void *) const'
         spec.ignore 'wxDataObjectSimple::SetData(size_t, const void *)'
         spec.ignore 'wxDataObjectSimple::GetDataSize() const'
-        # these are missing in the XML doc but fully implemented here
-        # add them, so SWIG does not think this is an abstract class
-        spec.extend_interface('wxDataObjectSimple',
-                              'virtual void GetAllFormats(wxDataFormat *formats, Direction dir=Get) const',
-                              'virtual bool GetDataHere(const wxDataFormat &format, void *buf) const',
-                              'virtual size_t GetDataSize(const wxDataFormat &format) const',
-                              'virtual size_t GetFormatCount(Direction dir=Get) const',
-                              'virtual wxDataFormat GetPreferredFormat(Direction dir=Get) const')
-        # for these 'final' classes forget about any of the virtual methods since these
-        # were fully implemented in the base and should not be overridden in Ruby
-        %w[wxBitmapDataObject].each do |kls|
-          spec.no_proxy %W[
-            #{kls}::GetFormatCount
-            #{kls}::GetPreferredFormat
-            #{kls}::GetAllFormats
-            #{kls}::GetDataHere
-            #{kls}::GetDataSize
-            #{kls}::SetData
-            ]
+        # ignore this as all derivatives have a fixed format
+        spec.ignore 'wxDataObjectSimple::SetFormat'
+
+        # ignore these as all fully implemented in derived and not useful in Ruby
+        spec.ignore 'wxCustomDataObject::Alloc'
+        spec.ignore 'wxCustomDataObject::Free'
+        spec.ignore 'wxCustomDataObject::GetData'
+        spec.ignore 'wxCustomDataObject::SetData'
+        spec.ignore 'wxCustomDataObject::GetSize'
+        spec.ignore 'wxCustomDataObject::TakeData'
+
+        # all available in bases
+        spec.ignore 'wxTextDataObject::GetFormatCount'
+        spec.ignore 'wxTextDataObject::GetFormat'
+        spec.ignore 'wxTextDataObject::GetAllFormats'
+
+        %w[wxBitmapDataObject wxFileDataObject wxTextDataObject wxImageDataObject wxURLDataObject].each do |kls|
+          spec.add_swig_code <<~__HEREDOC
+            // SWIG gets confused and doesn't realise that various virtual methods
+            // from wxDataObject are implemented fully in this subclass, and so,
+            // believing it to be abstract doesn't provide an allocator for this
+            // class. This undocumented feature overrides this.
+            %feature("notabstract") #{kls};
+            __HEREDOC
         end
       end
     end # class DataObject
