a0ae258a23df1f8a2655a384aab9f3c55e811e26
Martin Corino <mcorino@m2c-software.nl>
2022-12-09 12:10:10 +0100

    replace type map declarations

--- rake/lib/director/styled_text_ctrl.rb
+++ rake/lib/director/styled_text_ctrl.rb
@@ -18,31 +18,23 @@ module WXRuby3
       def setup
         super
         spec.ignore_bases('wxStyledTextCtrl' => 'wxTextEntry')
-        spec.add_swig_code <<~__HEREDOC
-          %typemap(in,numinputs=0) (int *OUTPUT) (int a)
-          {
-            $1 = &a;
-          }
-          
-          %typemap(argout) (int *OUTPUT)
-          {
+        spec.map 'int *OUTPUT' do
+          map_type 'Integer'
+          map_in ignore: true, temp: 'int a', code: '$1 = &a;'
+          map_argout code: <<~__CODE
             $result = rb_ary_new();
             rb_ary_push($result, INT2NUM(*$1));
-          }
-          
-          %typemap(in,numinputs=0) (int *OUTPUT, int *OUTPUT) (int a, int b)
-          {
-            $1 = &a;
-            $2 = &b;
-          }
-          
-          %typemap(argout) (int *OUTPUT, int *OUTPUT)
-          {
+            __CODE
+        end
+        spec.map 'int *OUTPUT, int *OUTPUT' do
+          map_type 'Array<Integer>'
+          map_in ignore: true, temp: 'int a, int b', code: '$1 = &a; $2 = &b;'
+          map_argout code: <<~__CODE
             $result = rb_ary_new();
             rb_ary_push($result, INT2NUM(*$1));
             rb_ary_push($result, INT2NUM(*$2));
-          }
-          __HEREDOC
+          __CODE
+        end
         spec.do_not_generate(:variables, :enums, :defines, :functions)
       end
     end # class StyledTextCtrl
