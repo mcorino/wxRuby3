995bf9490347038880fa7731b684fdf202428a8c
Martin Corino <mcorino@m2c-software.nl>
2022-12-09 20:11:22 +0100

    replace type map declarations

--- rake/lib/director/ctrl_with_items.rb
+++ rake/lib/director/ctrl_with_items.rb
@@ -44,19 +44,20 @@ module WXRuby3
               return item_data;
             }
             __HEREDOC
-          )
-          spec.add_swig_code <<~__HEREDOC
-            // Typemap for GetStrings - which returns an object not a reference,
-            // unlike all other ArrayString-returning methods
-            %typemap(out) wxArrayString {
+            )
+          # Typemap for GetStrings - which returns an object not a reference,
+          # unlike all other ArrayString-returning methods
+          spec.map 'wxArrayString' do
+            map_type 'Array<String>'
+            map_out code: <<~__CODE
               $result = rb_ary_new();
               for (size_t i = 0; i < $1.GetCount(); i++)
               {
                 rb_ary_push($result, WXSTR_TO_RSTR($1.Item(i)));
               }
-            }
-            __HEREDOC
-        end
+              __CODE
+          end
+       end
       end
 
       def setup_ctrl_with_items(clsnm)
