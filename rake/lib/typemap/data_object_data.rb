e52fe0d3791fe4917ec68991a627727dfc51c5bb
Martin Corino <mcorino@m2c-software.nl>
2022-12-13 19:40:50 +0100

    cleanup by using proper type maps

--- rake/lib/typemap/data_object_data.rb
+++ rake/lib/typemap/data_object_data.rb
@@ -19,33 +19,66 @@ module WXRuby3
         # For wxDataObject::GetDataHere: the ruby method is passed the DataFormat
         # for the sought data, and should return either a string containing the
         # data, or nil if the data cannot be provided for some reason.
-        map 'const wxDataFormat& format, void *buf' do
+        map 'const wxDataFormat &format, void *buf' do
           # this is needed for this and all other mappings
           add_header '#include <wx/dataobj.h>'
+          # add include for unique_ptr<>
+          add_header '#include <memory>'
 
           map_type type: 'Wx::DataFormat', name: 0
+
+          map_in temp: 'std::unique_ptr<char> data_buf, size_t data_size', code: <<~__CODE
+            void* argp$argnum = NULL;
+            if ( TYPE($input) == T_DATA )
+            {
+              if (SWIG_IsOK(SWIG_ConvertPtr($input, &argp$argnum, $1_descriptor, $argnum-1)) && argp$argnum)
+              {
+                $1 = reinterpret_cast< $1_basetype * >(argp$argnum);
+              }
+            }
+            if (!argp$argnum)
+            {
+              rb_raise(rb_eTypeError, "Expected Wx::DataFormat instance.");
+            }
+            data_size = arg1->GetDataSize(*$1);
+            data_buf.reset(new char[data_size]);
+            $2 = data_buf.get ();
+            __CODE
+
+          # ignore C defined return value entirely (also affects directorout)
+          map_out ignore: 'bool'
+
+          map_argout code: <<~__CODE
+            if (result)
+            {
+              $result = rb_str_new( (const char*)data_buf$argnum.get(), data_size$argnum);
+            }
+            else
+              $result = Qnil;
+            __CODE
+
           # convert the dataformat and ignore the buffer pointer for now
           map_directorin code: '$input = SWIG_NewPointerObj(SWIG_as_voidptr(&$1), SWIGTYPE_p_wxDataFormat, 0);'
-        end
 
-        map 'WXRUBY_DATA_OUT' do
-          map_type 'String'
-          map_directorout code: <<~__CODE
-            if ( RTEST($1) )
-              if ( TYPE($1) == T_STRING )
-                {
-                  memcpy(buf, StringValuePtr($1), RSTRING_LEN($1) );
-                  $result = true;
-                }
+          map_directorargout code: <<~__CODE
+            if (RTEST(result))
+            {
+              if (TYPE(result) == T_STRING)
+              {
+                memcpy(buf, StringValuePtr(result), RSTRING_LEN(result) );
+                c_result = true;
+              }
               else
-                {
-                  rb_raise(rb_eTypeError, 
-                           "get_data_here should return a string, or nil on failure");
-                  $result = false;
-                }
+              {
+                rb_raise(rb_eTypeError, 
+                         "get_data_here should return a string, or nil on failure");
+                c_result = false;
+              }
+            }
             else
-              $result = false;
+              c_result = false;
           __CODE
+
         end
 
         # For SetData: the data contents to be set upon the data object is
@@ -53,13 +86,15 @@ module WXRuby3
         # value if the data could be set successfully, or false/nil if it could
         # not. This string is marked as tainted.
         map 'size_t len, const void* buf' do
-          map_type type: 'String', name: 1
-          map_directorin code: '$input = rb_external_str_new( (const char *)buf, len );'
-        end
 
-        map 'WXRUBY_DATA_IN' do
-          map_type 'true,false'
-          map_directorout code: '$result = RTEST($1);'
+          map_type type: 'String', name: 1
+
+          map_in code: <<~__CODE
+            $1 = RSTRING_LEN($input);
+            $2 = (void*)StringValuePtr($input);
+            __CODE
+
+          map_directorin code: '$input = rb_external_str_new( (const char *)buf, len );'
         end
 
       end # define
