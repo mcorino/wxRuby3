da0737cede0b4670ef9a0571871dbbcbc4e89104
Martin Corino <mcorino@m2c-software.nl>
2022-12-13 12:45:49 +0100

    remove typemaps for deprecated wxTextAttrEx; complete typemaps for wxRichTextRange

--- rake/lib/typemap/richtext.rb
+++ rake/lib/typemap/richtext.rb
@@ -37,45 +37,28 @@ module WXRuby3
             $1 = &rng;
             __CODE
 
-          map_typecheck code: '$1 = ( CLASS_OF($input) == rb_cRange );'
+          map_typecheck precedence: 1, code: '$1 = ( CLASS_OF($input) == rb_cRange );'
 
-          map_out code: '$result = rb_range_new (LONG2NUM($1->GetStart()),LONG2NUM($1->GetEnd()),0);'
+          map_directorin code: '$input = rb_range_new (LONG2NUM($1.GetStart()),LONG2NUM($1.GetEnd()),0);'
+
+        end
+
+        map 'wxRichTextRange' do
+
+          map_type 'Range'
+
+          map_out code: '$result = rb_range_new (LONG2NUM($1.GetStart()),LONG2NUM($1.GetEnd()),0);'
+
+          map_directorout code: <<~__CODE
+            int start = NUM2INT( rb_funcall( $input, rb_intern("begin"), 0));
+            int end   = NUM2INT( rb_funcall( $input, rb_intern("end"), 0));
+            $result = wxRichTextRange(start, end);
+            __CODE
 
         end
 
         # Used as in/out parameters by some other selection-getting methods
         map_apply 'int *OUTPUT' => [ 'long * from' , 'long * to' ]
-
-        # For some reason, some methods in RichTextCtrl accept and return
-        # TextAttrEx, some RichTextAttr and some both. For those that support
-        # both, the TextAttrEx versions are ignored in the class's individual
-        # interface file. The typemaps below convert those that only accept or
-        # return TextAttrEx in C++ to accept/return Wx::RichTextAttr objects
-        # from Ruby, so the TextAttrEx class remains unported.
-        map 'wxTextAttrEx&' do
-
-          map_type 'Wx::RichTextAttr'
-
-          map_in code: <<~__CODE
-            void *arg = 0;
-            int result = SWIG_ConvertPtr($input, &arg, SWIGTYPE_p_wxRichTextAttr, 0);
-            wxRichTextAttr* rich_attr = reinterpret_cast< wxRichTextAttr* >(arg);
-            wxTextAttrEx attr_ex(*rich_attr);
-            $1 = &attr_ex;
-            __CODE
-
-          map_typecheck code: <<~__CODE
-            void *arg = 0;
-            int result = SWIG_ConvertPtr($input, &arg, SWIGTYPE_p_wxRichTextAttr, 0);
-            $1 = SWIG_CheckState(res);
-            __CODE
-
-          map_out code: <<~__CODE
-            wxRichTextAttr* rta = new wxRichTextAttr($1);
-            $result = SWIG_NewPointerObj(rta, SWIGTYPE_p_wxRichTextAttr, SWIG_POINTER_OWN);
-            __CODE
-
-        end
 
       end # define
 
