2669fa81ce271c47f92522fd37ec34cdb05c29c7
Martin Corino <mcorino@m2c-software.nl>
2023-01-02 15:04:39 +0100

    fix typo

--- lib/wx/doc/aui/auimanager.rb
+++ lib/wx/doc/aui/auimanager.rb
@@ -6,7 +6,7 @@ class Wx::Aui::AuiManager
   def each_pane; end
 
   # Returns an array of all panes managed by the frame manager.
-  # @return [Array<Wx::Aui::AuiPaneInfo] all managed panes
+  # @return [Array<Wx::Aui::AuiPaneInfo>] all managed panes
   def get_all_panes; end
   alias_method :all_panes, :get_all_panes
 end
