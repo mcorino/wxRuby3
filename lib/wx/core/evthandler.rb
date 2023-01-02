02d970549530ca4c54f1f7f3d78bee60258505fd
Martin Corino <mcorino@m2c-software.nl>
2023-01-02 20:34:48 +0100

    documentation updates

--- lib/wx/core/evthandler.rb
+++ lib/wx/core/evthandler.rb
@@ -162,10 +162,6 @@ class Wx::EvtHandler
   private :acquire_id, :acquire_handler
 
   # Process a command, supplying the window identifier, command event identifier, and member function or proc.
-  # @param [Integer] id window identifier
-  # @param [Integer] evt_id event type identifier
-  # @param [String,Symbol,Method,Proc] meth (name of) method or event handling proc
-  # @yieldparam [Wx::CommandEvent] event event to handle
   def evt_command(id, evt_id, meth = nil, &block)
     handler = acquire_handler(meth, block)
     id  = acquire_id(id)
@@ -173,11 +169,6 @@ class Wx::EvtHandler
   end
 
   # Process a command for a range of window identifiers, supplying the minimum and maximum window identifiers, command event identifier, and member function or proc.
-  # @param [Integer] id1 minimum window identifier
-  # @param [Integer] id2 maximum window identifier
-  # @param [Integer] evt_id event type identifier
-  # @param [String,Symbol,Method,Proc] meth (name of) method or event handling proc
-  # @yieldparam [Wx::CommandEvent] event event to handle
   def evt_command_range(id1, id2, evt_id, meth = nil, &block)
     handler = acquire_handler(meth, block)
     id  = acquire_id(id)
@@ -185,7 +176,6 @@ class Wx::EvtHandler
   end
 
   # Convenience evt_handler to listen to all mouse events.
-  # @yieldparam [Wx::MouseEvent] event event to handle
   def evt_mouse_events(&block)
     evt_left_down(&block)
     evt_left_up(&block)
@@ -203,7 +193,6 @@ class Wx::EvtHandler
   end
 
   # Convenience evt handler to listen to all scrollwin events.
-  # @yieldparam [Wx::ScrollWinEvent] event event to handle
   def evt_scrollwin(&block)
     evt_scrollwin_top(&block)
     evt_scrollwin_bottom(&block)
