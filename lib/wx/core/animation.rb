d2699ab3b04af191f2c84042e018db9f4397cbf5
Martin Corino <mcorino@m2c-software.nl>
2023-01-16 21:45:45 +0100

    Animation customization

---
+++ lib/wx/core/animation.rb
@@ -0,0 +1,18 @@
+class Wx::Animation
+  # Redefine the initialize method so it raises an exception if a
+  # non-existent file is given to the constructor; otherwise, wx Widgets
+  # just carries on with an empty bitmap, which may cause faults later
+  wx_init = self.instance_method(:initialize)
+  define_method(:initialize) do | *args |
+    if args[0].kind_of? String
+      if not File.exist?( File.expand_path(args[0]) )
+        Kernel.raise( ArgumentError, 
+                      "Animation file does not exist: #{args[0]}" )
+      end
+      res = wx_init.bind(self).call()
+      res.load_file(args[0], args[1] || Wx::ANIMATION_TYPE_ANY)
+    else
+      wx_init.bind(self).call(*args)
+    end
+  end
+end
