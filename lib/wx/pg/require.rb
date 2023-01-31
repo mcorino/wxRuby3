7748f0df69c5921930d3cb238836887d20f29a15
Martin Corino <mcorino@m2c-software.nl>
2023-01-31 19:50:00 +0100

    load all modules

--- lib/wx/pg/require.rb
+++ lib/wx/pg/require.rb
@@ -2,4 +2,7 @@
 # Copyright (c) M.J.N. Corino, The Netherlands
 
 require_relative './events/evt_list'
+require_relative './property_grid_interface'
 require_relative './property_grid'
+require_relative './pg_property'
+require_relative './pgarray_editor_dialog'
