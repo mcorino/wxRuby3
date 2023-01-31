55d059b9e7bb7d6777835585130f5986d380a1d8
Martin Corino <mcorino@m2c-software.nl>
2023-01-31 12:21:12 +0100

    return enumerator if no block given

--- lib/wx/pg/pg_property.rb
+++ lib/wx/pg/pg_property.rb
@@ -6,4 +6,18 @@ module Wx::PG
   NullProperty = nil
   PGChoicesEmptyData = nil
 
+  class PGProperty
+
+    wx_each_attribute = instance_method :each_attribute
+    define_method :each_attribute do |id|
+      if block_given?
+        wx_each_attribute.bind(self).call(id)
+      else
+        ::Enumerator.new { |y| wx_each_attribute.bind(self).call(id) { |variant| y << variant } }
+      end
+    end
+    alias :attributes :each_attribute
+
+  end
+
 end
