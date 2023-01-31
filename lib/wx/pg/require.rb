5e4873a5f368c663f524673679e90cbebc6f5cdf
Martin Corino <mcorino@m2c-software.nl>
2023-01-31 14:27:53 +0100

    include event list

--- lib/wx/pg/require.rb
+++ lib/wx/pg/require.rb
@@ -1,6 +1,5 @@
 # Wx::PG sub package for wxRuby3
 # Copyright (c) M.J.N. Corino, The Netherlands
 
-#require_relative './ext'
-#require_relative './events/evt_list'
+require_relative './events/evt_list'
 require_relative './property_grid'
