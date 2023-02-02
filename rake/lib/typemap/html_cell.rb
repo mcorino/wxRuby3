a28d04c2a8bd1ffb0b0c7e476b552dc29c868b81
Martin Corino <mcorino@m2c-software.nl>
2023-02-02 13:05:59 +0100

    also add directorin mapping

--- rake/lib/typemap/html_cell.rb
+++ rake/lib/typemap/html_cell.rb
@@ -32,6 +32,20 @@ module WXRuby3
               $result = SWIG_NewPointerObj($1, swig_type, 0);
             }
             __CODE
+          map_directorin code: <<~__CODE
+            // attempt a dynamic cast to wxHtmlContainerCell 
+            wxHtmlContainerCell* con_cell = dynamic_cast<wxHtmlContainerCell*> ($1);
+            if (con_cell)
+            {
+              swig_type_info* swig_type = wxRuby_GetSwigTypeForClass(rb_eval_string("Wx::Html::HtmlContainerCell"));
+              $input = SWIG_NewPointerObj(con_cell, swig_type, 0);
+            }
+            else
+            {
+              swig_type_info* swig_type = wxRuby_GetSwigTypeForClass(rb_eval_string("Wx::Html::HtmlCell"));
+              $input = SWIG_NewPointerObj($1, swig_type, 0);
+            }
+            __CODE
         end
 
       end
