696ebe4872afb22e4b7ea9b489e7405c0e358355
Martin Corino <mcorino@m2c-software.nl>
2023-02-03 15:35:58 +0100

    add some constants misssing from docs

--- lib/wx/doc/grids/grid.rb
+++ lib/wx/doc/grids/grid.rb
@@ -1,7 +1,20 @@
 
-class Wx::Grids::Grid
+module Wx::Grids
 
-  alias :set_table :assign_table
-  alias :table= :assign_table
+  GRID_VALUE_STRING = 'string'
+  GRID_VALUE_BOOL = 'bool'
+  GRID_VALUE_NUMBER = 'long'
+  GRID_VALUE_FLOAT = 'double'
+  GRID_VALUE_CHOICE = 'choice'
+  GRID_VALUE_DATE = 'date'
+  GRID_VALUE_TEXT = GRID_VALUE_STRING
+  GRID_VALUE_LONG = GRID_VALUE_NUMBER
+  
+  class Grid
 
+    alias :set_table :assign_table
+    alias :table= :assign_table
+
+  end
+  
 end
