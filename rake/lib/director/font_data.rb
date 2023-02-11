eb1f129fe86aecff1cf66c16c0743c7adbabbe12
Martin Corino <mcorino@m2c-software.nl>
2023-02-11 13:18:03 +0100

    add director to customize wxFontData

---
+++ rake/lib/director/font_data.rb
@@ -0,0 +1,22 @@
+###
+# wxRuby3 wxWidgets interface director
+# Copyright (c) M.J.N. Corino, The Netherlands
+###
+
+module WXRuby3
+
+  class Director
+
+    class FontData < Director
+
+      def setup
+        super
+        # add copy ctor missing from XML docs
+        spec.extend_interface 'wxFontData',
+                              'wxFontData(const wxFontData & other)'
+      end
+    end # class FontData
+
+  end # class Director
+
+end # module WXRuby3
