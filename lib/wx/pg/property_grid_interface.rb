57f1acf9cdfd109f636c2c15cb80ad62dc99bbd1
Martin Corino <mcorino@m2c-software.nl>
2023-02-19 20:26:57 +0100

    fix iterators

--- lib/wx/pg/property_grid_interface.rb
+++ lib/wx/pg/property_grid_interface.rb
@@ -4,9 +4,9 @@ module Wx::PG
   module PropertyGridInterface
 
     wx_each_property = instance_method :each_property
-    define_method :each_property do |flags = Wx::PG::PG_ITERATE_DEFAULT, start = nil, recurse: true|
-      if block_given?
-        wx_each_property.bind(self).call(flags.to_int, start, recurse)
+    define_method :each_property do |flags = Wx::PG::PG_ITERATE_DEFAULT, start = nil, recurse: true, &block|
+      if block
+        wx_each_property.bind(self).call(flags.to_int, start, recurse, &block)
       else
         ::Enumerator.new { |y| wx_each_property.bind(self).call(flags.to_int, start, recurse) { |prop| y << prop } }
       end
@@ -14,9 +14,9 @@ module Wx::PG
     alias :properties :each_property
 
     wx_each_property_attribute = instance_method :each_property_attribute
-    define_method :each_property_attribute do |id|
-      if block_given?
-        wx_each_property_attribute.bind(self).call(id)
+    define_method :each_property_attribute do |id, &block|
+      if block
+        wx_each_property_attribute.bind(self).call(id, &block)
       else
         ::Enumerator.new { |y| wx_each_property_attribute.bind(self).call(id) { |variant| y << variant } }
       end
