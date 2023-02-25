f37307a55a291ffe6f084e7948c7fb923fe2d4a6
Martin Corino <mcorino@m2c-software.nl>
2023-02-25 13:58:05 +0100

    add RibbonArtProvider GC management

---
+++ lib/wx/rbn/ribbon_control.rb
@@ -0,0 +1,15 @@
+
+class Wx::RBN::RibbonControl
+
+  # manage RibbonArtProvider for GC
+  wx_set_art_provider = instance_method :set_art_provider
+  define_method :set_art_provider do |prov|
+    @art_provider = nil # clear any previously set
+    wx_set_art_provider.bind(self).call(prov)
+    if prov != get_ancestor_ribbon_bar.get_art_provider
+      @art_provider = prov # keep non-wx-managed provider safe
+    end
+  end
+  alias :art_provider= :set_art_provider
+
+end
