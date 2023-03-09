30e665ab01ea3b147b8dbf6801ef306e76bf5b09
Martin Corino <mcorino@m2c-software.nl>
2023-03-09 09:46:55 +0100

    no .bat binaries needed anymore

--- rakelib/bin.rake
+++ rakelib/bin.rake
@@ -24,7 +24,7 @@ namespace :wxruby do
       WXRuby3::Bin.binaries.each do |bin|
         if File.exist?(File.join('bin', bin))
           content = IO.read(File.join('bin', bin))
-          rm_f(File.join('bin', bin)) unless content == WXRuby3::Bin.__send__(bin.gsub('.','_').to_sym)
+          rm_f(File.join('bin', bin)) unless content == WXRuby3::Bin.__send__(bin.to_sym)
         end
       end
     end
@@ -34,14 +34,3 @@ namespace :wxruby do
 end
 
 CLOBBER.include File.join('bin', 'wxruby')
-
-if WXRuby3.config.windows?
-
-  file File.join('bin', 'wxruby.bat') => ['bin'] do |t|
-    File.open(t.name, 'w') { |f| f.puts WXRuby3::Bin.wxruby_bat }
-  end
-  Rake::Task['wxruby:bin:files'].enhance [File.join('bin', 'wxruby.bat')]
-
-  CLOBBER.include File.join('bin', 'wxruby.bat')
-
-end
--- rakelib/bin.rb
+++ rakelib/bin.rb
@@ -24,35 +24,8 @@ module WXRuby3
           _SH_TXT
       end
 
-      def wxruby_bat
-        <<~_BAT_TXT
-          @echo off
-          if not "%~f0" == "~f0" goto WinNT
-          #{RB_CONFIG['ruby_install_name']} -Sx "%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
-          goto endofruby
-          :WinNT
-          if not exist "%~d0%~p0#{RB_CONFIG['ruby_install_name']}" goto rubyfrompath
-          if exist "%~d0%~p0#{RB_CONFIG['ruby_install_name']}" "%~d0%~p0#{RB_CONFIG['ruby_install_name']}" -x "%~f0" %*
-          goto endofruby
-          :rubyfrompath
-          #{RB_CONFIG['ruby_install_name']} -x "%~f0" %*
-          goto endofruby
-          #!/bin/#{RB_CONFIG['ruby_install_name']}
-          #
-          module WxRuby
-            ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-          end
-          require 'wx/wxruby/base'
-          WxRuby.run
-          __END__
-          :endofruby
-          _BAT_TXT
-      end
-
       def binaries
-        l = %w{wxruby}
-        l.concat %w{wxruby.bat} if WXRuby3.config.windows?
-        l
+        %w{wxruby}
       end
     end
 
