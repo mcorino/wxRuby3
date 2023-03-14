765d4d9bdec4b703cb867ffcdf873cf4382d9d72
Martin Corino <mcorino@m2c-software.nl>
2023-03-14 14:41:15 +0100

    do not run embedded; AuiManager seems to have some serious problems

--- samples/aui/aui.rb
+++ samples/aui/aui.rb
@@ -1306,16 +1306,17 @@ module AUISample
       description: 'wxRuby example demonstrating the AUI framework.')
   end
 
-  def self.activate
-    frame = AuiFrame.new(nil, Wx::ID_ANY, "Wx::AUI Sample Application",
-                         Wx::DEFAULT_POSITION,
-                         Wx::Size.new(800, 600))
-    frame.show
-    frame
+  def self.run
+    execute(__FILE__)
   end
 
   if $0 == __FILE__
-    Wx::App.run { AUISample.activate }
+    Wx::App.run do
+      frame = AuiFrame.new(nil, Wx::ID_ANY, "Wx::AUI Sample Application",
+                           Wx::DEFAULT_POSITION,
+                           Wx::Size.new(800, 600))
+      frame.show
+    end
   end
 
 end
