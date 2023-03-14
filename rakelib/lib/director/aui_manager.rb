2d28c5236634ec8b4bf58af840d172ad62889b37
Martin Corino <mcorino@m2c-software.nl>
2023-03-14 21:31:55 +0100

    fix GC handling

--- rakelib/lib/director/aui_manager.rb
+++ rakelib/lib/director/aui_manager.rb
@@ -16,6 +16,10 @@ module WXRuby3
         spec.gc_as_object
         # need a custom implementation to handle event handler proc cleanup
         spec.add_header_code <<~__HEREDOC
+          #include "wx/aui/aui.h"
+
+          WXRUBY_EXPORT void GC_SetWindowDeleted(void *ptr);
+
           class WXRubyAuiManager : public wxAuiManager
           {
           public:
@@ -24,6 +28,36 @@ module WXRuby3
             virtual ~WXRubyAuiManager() 
             {
               wxRuby_ReleaseEvtHandlerProcs(this);
+            }
+
+            void OnManagedWindowClose(wxCloseEvent& event)
+            {
+              UnInit();
+              for ( size_t i = 0; i < m_panes.size(); i++ )
+              {
+                  wxAuiPaneInfo& pinfo = m_panes[i];
+                  if (pinfo.window)
+                  {
+                      GC_SetWindowDeleted(pinfo.window);
+                      delete pinfo.window;
+                  }
+              }
+              m_panes.Clear();
+              m_docks.Clear();
+              m_uiParts.Clear();
+              delete m_art;
+              m_action = actionNone;
+              m_actionWindow = NULL;
+              m_hoverButton = NULL;
+              m_art = new wxAuiDefaultDockArt;
+              m_hintWnd = NULL;
+              m_flags = wxAUI_MGR_DEFAULT;
+              m_hasMaximized = false;
+              m_dockConstraintX = 0.3;
+              m_dockConstraintY = 0.3;
+              m_reserved = NULL;
+              m_currentDragItem = -1;
+              event.Skip();
             }               
           };
         __HEREDOC
@@ -43,6 +77,7 @@ module WXRuby3
         spec.add_swig_code '%markfunc wxAuiManager "GC_mark_wxAuiManager";'
         # provide pure Ruby implementation based on use custom alternative provided below
         spec.ignore('wxAuiManager::GetAllPanes')
+        spec.ignore('wxAuiManager::SetManagedWindow', ignore_doc: false)
         spec.add_extend_code 'wxAuiManager', <<~__HEREDOC
           VALUE each_pane() 
           {
@@ -56,6 +91,13 @@ module WXRuby3
               rc = rb_yield(r_pi);
             }	
             return rc;
+          }
+
+          void SetManagedWindow(wxWindow* managedWnd)
+          {
+            self->SetManagedWindow(managedWnd);
+            WXRubyAuiManager* aui_mng = dynamic_cast<WXRubyAuiManager*> (self);
+            managedWnd->Bind(wxEVT_CLOSE_WINDOW, &WXRubyAuiManager::OnManagedWindowClose, aui_mng);
           }
           __HEREDOC
         spec.suppress_warning(473, 'wxAuiManager::CreateFloatingFrame')
