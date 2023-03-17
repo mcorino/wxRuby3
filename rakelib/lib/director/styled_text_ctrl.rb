f058734f72596f5d3f015cf543c393cb99fcd771
Martin Corino <mcorino@m2c-software.nl>
2023-03-17 11:04:36 +0100

    improve and cleanup StyledTestCtrl wrapper

--- rakelib/lib/director/styled_text_ctrl.rb
+++ rakelib/lib/director/styled_text_ctrl.rb
@@ -14,21 +14,65 @@ module WXRuby3
       def setup
         super
         spec.override_inheritance_chain('wxStyledTextCtrl', %w[wxControl wxWindow wxEvtHandler wxObject])
-        spec.map 'int *OUTPUT' => 'Integer' do
-          map_in ignore: true, temp: 'int a', code: '$1 = &a;'
+        spec.map 'int *', 'long *', as: 'Integer' do
+          map_in ignore: true, temp: '$*1_ltype a', code: '$1 = &a;'
           map_argout code: <<~__CODE
-            $result = rb_ary_new();
-            rb_ary_push($result, INT2NUM(*$1));
+            if (NIL_P($result)) $result = INT2NUM(*$1);
+            else 
+            {
+              if (TYPE($result) != T_ARRAY)
+              {
+                VALUE rc = rb_ary_new();
+                rb_ary_push(rc, $result);
+                $result = rc;
+              }
+              rb_ary_push($result, INT2NUM(*$1));
+            }
             __CODE
         end
-        spec.map 'int *OUTPUT, int *OUTPUT' => 'Array<Integer>' do
-          map_in ignore: true, temp: 'int a, int b', code: '$1 = &a; $2 = &b;'
+        spec.map 'long *, long *', 'wxTextCoord *col, wxTextCoord *row', as: 'Array<Integer>' do
+          map_in ignore: true, temp: '$*1_ltype a, $*2_ltype b', code: '$1 = &a; $2 = &b;'
           map_argout code: <<~__CODE
-            $result = rb_ary_new();
+            if (TYPE($result) != T_ARRAY)
+            {
+              VALUE rc = rb_ary_new();
+              if (!NIL_P($result)) rb_ary_push(rc, $result);
+              $result = rc;
+            }
             rb_ary_push($result, INT2NUM(*$1));
             rb_ary_push($result, INT2NUM(*$2));
           __CODE
         end
+        # not useful in wxRuby
+        spec.ignore 'wxStyledTextCtrl::HitTest(const wxPoint &, long *) const',
+                    'wxStyledTextCtrl::GetDirectFunction',
+                    'wxStyledTextCtrl::GetDirectPointer',
+                    'wxStyledTextCtrl::CreateLoader',
+                    'wxStyledTextCtrl::AddTextRaw',
+                    'wxStyledTextCtrl::InsertTextRaw',
+                    'wxStyledTextCtrl::GetCurLineRaw',
+                    'wxStyledTextCtrl::GetLineRaw',
+                    'wxStyledTextCtrl::GetSelectedTextRaw',
+                    'wxStyledTextCtrl::GetTargetTextRaw',
+                    'wxStyledTextCtrl::GetTextRangeRaw',
+                    'wxStyledTextCtrl::SetTextRaw',
+                    'wxStyledTextCtrl::GetTextRaw',
+                    'wxStyledTextCtrl::AppendTextRaw',
+                    'wxStyledTextCtrl::ReplaceSelectionRaw',
+                    'wxStyledTextCtrl::ReplaceTargetRaw',
+                    'wxStyledTextCtrl::ReplaceTargetRERaw',
+                    'wxStyledTextCtrl::SetStyleBytes',
+                    'wxStyledTextCtrl::RegisterImage(int, const char *const *)'
+        # TODO : these need investigating to see if they might be useful
+        spec.ignore 'wxStyledTextCtrl::GetDocPointer',
+                    'wxStyledTextCtrl::SetDocPointer',
+                    'wxStyledTextCtrl::CreateDocument',
+                    'wxStyledTextCtrl::AddRefDocument',
+                    'wxStyledTextCtrl::ReleaseDocument',
+                    'wxStyledTextCtrl::PrivateLexerCall'
+        # TODO : these will need some sort of stream solution to be useful
+        spec.ignore 'wxStyledTextCtrl::GetCharacterPointer',
+                    'wxStyledTextCtrl::GetRangePointer'
         spec.do_not_generate(:variables, :enums, :defines, :functions)
       end
     end # class StyledTextCtrl
