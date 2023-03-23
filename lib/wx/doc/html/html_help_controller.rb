1a7c304d7305036328a51ed732f47c6733ccc248
Martin Corino <mcorino@m2c-software.nl>
2023-03-23 16:26:44 +0100

    use correct 'type' name

--- lib/wx/doc/html/html_help_controller.rb
+++ lib/wx/doc/html/html_help_controller.rb
@@ -2,7 +2,7 @@
 class Wx::HTML::HelpController
 
   # Returns the latest frame size and position settings and whether a new frame is drawn with each invocation.
-  # @return [Array(Wx::Frame,Wx::Size,Wx::Point,bool)] latest frame settings
+  # @return [Array(Wx::Frame,Wx::Size,Wx::Point,Boolean)] latest frame settings
   def get_frame_parameters; end
 
 end
