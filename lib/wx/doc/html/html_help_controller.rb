c7a68a38aaa238e75e59a0642737129ea17cd88c
Martin Corino <mcorino@m2c-software.nl>
2023-03-23 14:58:23 +0100

    fix GetFrameParameters; add ExtHelpController

---
+++ lib/wx/doc/html/html_help_controller.rb
@@ -0,0 +1,8 @@
+
+class Wx::HTML::HelpController
+
+  # Returns the latest frame size and position settings and whether a new frame is drawn with each invocation.
+  # @return [Array(Wx::Frame,Wx::Size,Wx::Point,bool)] latest frame settings
+  def get_frame_parameters; end
+
+end
