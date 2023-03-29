ca7752dd96511ce990ff9f313ac007799267725a
Martin Corino <mcorino@m2c-software.nl>
2023-03-29 14:25:42 +0200

    add Yard extension

---
+++ rakelib/yard/yard-relative_markdown_links.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+# Adapted from the 'yard-relative_markdown_links' gem; Copyright © 2018 Andrew Haines
+
+require_relative 'yard/relative_markdown_links'
---
+++ rakelib/yard/yard/relative_markdown_links/version.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module YARD
+  module RelativeMarkdownLinks
+    # Current version of the yard-relative_markdown_links gem.
+    VERSION = "0.4.1"
+  end
+end
