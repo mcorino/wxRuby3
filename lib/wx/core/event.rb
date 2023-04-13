bcc69bbf1d1ffce1b55c9b3f898ee6980c2bde80
Martin Corino <mcorino@m2c-software.nl>
2023-04-13 12:24:37 +0200

    handle unpublished EVT_NC_PAINT/NcPaintEvent

--- lib/wx/core/event.rb
+++ lib/wx/core/event.rb
@@ -3,3 +3,14 @@ class Wx::Event
   # Get the Wx id, not Ruby's deprecated Object#id
   alias :id :get_id
 end
+
+module Wx
+  # reduce mapping warnings for this unpublished event class
+  NcPaintEvent = Wx::Event
+
+  EvtHandler.register_event_type EvtHandler::EventType[
+    'evt_nc_paint', 0,
+    Wx::EVT_NC_PAINT,
+    Wx::NcPaintEvent
+  ]
+end
