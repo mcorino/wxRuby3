c4ef9f5e1a60ef76e2938c7ede44510a0e66498d
Martin Corino <mcorino@m2c-software.nl>
2023-04-30 10:05:27 +0200

    extend ControlWithItems item data tests

--- tests/test_item_data.rb
+++ tests/test_item_data.rb
@@ -31,6 +31,73 @@ class TestItemData < Test::Unit::TestCase
     assert_retrievable_data(f.control, 0, { 'b' => 'B' })
     assert_retrievable_data(f.control, 1, 'string item data')
     assert_retrievable_data(f.control, 2, 42.3)
+
+    GC.start
+
+    # single item append; no data
+    f.control.append('integer')
+    assert_equal(4, f.control.count)
+    assert_equal('integer', f.control.get_string(3))
+    assert_equal(nil, f.control.get_item_data(3))
+
+    # single item append; with data
+    f.control.append('array', 110)
+    assert_equal(5, f.control.count)
+    assert_equal('array', f.control.get_string(4))
+    assert_equal(110, f.control.get_item_data(4))
+
+    # array item append; no data
+    f.control.append(%w[set tree bag])
+    assert_equal(8, f.control.count)
+    assert_equal(nil, f.control.get_item_data(5))
+    assert_equal(nil, f.control.get_item_data(7))
+
+    # array item append; with data
+    f.control.append(%w[object module class], ['O', 'M', 'C'])
+    assert_equal(11, f.control.count)
+    assert_equal('O', f.control.get_item_data(8))
+    assert_equal('C', f.control.get_item_data(10))
+
+    GC.start
+
+    # single item insert; no data
+    f.control.insert('integer2', 3)
+    assert_equal(12, f.control.count)
+    assert_equal('integer2', f.control.get_string(3))
+    assert_equal(nil, f.control.get_item_data(3))
+
+    # single item insert; with data
+    f.control.insert('array2', 4, 110)
+    assert_equal(13, f.control.count)
+    assert_equal('array2', f.control.get_string(4))
+    assert_equal(110, f.control.get_item_data(4))
+
+    # array item insert; no data
+    f.control.insert(%w[set2 tree2 bag2], 5)
+    assert_equal(16, f.control.count)
+    assert_equal(nil, f.control.get_item_data(5))
+    assert_equal(nil, f.control.get_item_data(7))
+
+    # array item insert; with data
+    f.control.insert(%w[object2 module2 class2], 8, ['O', 'M', 'C'])
+    assert_equal(19, f.control.count)
+    assert_equal('O', f.control.get_item_data(8))
+    assert_equal('C', f.control.get_item_data(10))
+
+    GC.start
+
+    # item delete
+    f.control.delete(8)
+    assert_equal(18, f.control.count)
+    assert_equal('M', f.control.get_item_data(8))
+
+    GC.start
+
+    # clear all
+    f.control.clear
+    assert_equal(0, f.control.count)
+
+    GC.start
   end
 
   def test_treectrl_itemdata
