c8313d51ceebab2d84756a0db999a46b309c40fc
Martin Corino <mcorino@m2c-software.nl>
2023-04-30 12:02:04 +0200

    more multiplatform Icon support

--- lib/wx/core/imagelist.rb
+++ lib/wx/core/imagelist.rb
@@ -5,7 +5,7 @@ class Wx::ImageList
   wx_add = instance_method :add
   define_method :add do |*args|
     if Wx::Icon === args.first
-      args[0] = Wx::Bitmap.from_icon(args.first)
+      args[0] = args.first.to_bitmap
     end
     wx_add.bind(self).call(*args)
   end
