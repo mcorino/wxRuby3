07fc6daa21c44c22c17a845deccd6191733975c3
Martin Corino <mcorino@m2c-software.nl>
2023-05-26 10:30:42 +0200

    improve constants documentation gen

--- rakelib/lib/util/string.rb
+++ rakelib/lib/util/string.rb
@@ -88,7 +88,14 @@ module WXRuby3
 
       def rb_constant_value(name)
         val = name.sub(/\Awx/, 'Wx::')
-        (val == 'NULL' || val == 'nullptr') ? 'nil' : val
+        case val
+        when /NULL|nullptr/
+          'nil'
+        when /EmptyString/
+          %q['']
+        else
+          val
+        end
       end
 
       def rb_constant_expression(exp, const_xref)
@@ -136,11 +143,11 @@ module WXRuby3
                 ($1 == 'NULL' || $1 == 'nullptr') ? 'nil' : $1
               else
                 if const_xref.has_key?(rb_constant_name(idstr))
-                  "Wx::#{rb_constant_name(idstr)}"
+                  "#{const_xref[rb_constant_name(idstr)]['mod']}::#{rb_constant_name(idstr)}"
                 elsif const_xref.has_key?(rb_constant_name(idstr, false))
-                  "Wx::#{rb_constant_name(idstr, false)}"
+                  "#{const_xref[rb_constant_name(idstr, false)]['mod']}::#{rb_constant_name(idstr, false)}"
                 else
-                  "Wx::#{idstr}"
+                  rb_constant_value(idstr)
                 end
               end
             end
