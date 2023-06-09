5a2ee63b65f7e9e06a32080c85cc2f27aab8a081
Martin Corino <mcorino@m2c-software.nl>
2023-06-09 11:43:04 +0200

    make Event#clone return properly typed and managed object
    also force event classes to be non-abstract to get proper alloc funcs

--- lib/wx/doc/event.rb
+++ lib/wx/doc/event.rb
@@ -9,6 +9,16 @@ module Wx
     # @param [Wx::EventPropagation] prop_level
     def initialize(evt_type = Wx::EVT_NULL, id = 0, prop_level = Wx::EVENT_PROPAGATE_NONE) end
 
+    # Returns a copy of the event.
+    # Any event that is posted to the wxRuby event system for later action (via {Wx::EvtHandler#add_pending_event},
+    # {Wx::EvtHandler#queue_event} or {Wx::EvtHandler#post_event}) must implement this method.
+    # All standard wxRuby events fully implement this method and wxRuby has taken care of correctly handling this
+    # for any user defined event classes derived from either Wx::Event or Wx::CommandEvent.
+    # Creating user defined event classes derived for other classes than Wx::Event or Wx::CommandEvent is currently
+    # *not* supported in wxRuby.
+    # @return [Wx::Event]
+    def clone; end
+
   end
 
 end
