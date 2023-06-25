ed0805d7be9fbf06cf3d660d298a3c23da724803
Martin Corino <mcorino@m2c-software.nl>
2023-06-25 11:40:56 +0200

    use correct semantics

--- lib/wx/core/artprovider.rb
+++ lib/wx/core/artprovider.rb
@@ -7,7 +7,7 @@ class Wx::ArtProvider
     wx_push_back = instance_method(:push_back)
     define_method(:push_back) do | art_prov |
       wx_push_back.bind(self).call(art_prov)
-      @__art_provs.unshift(art_prov)
+      @__art_provs.push(art_prov)
     end
 
     wx_pop = instance_method(:pop)
@@ -19,7 +19,7 @@ class Wx::ArtProvider
     wx_push = instance_method(:push)
     define_method(:push) do | art_prov |
       wx_push.bind(self).call(art_prov)
-      @__art_provs.push(art_prov)
+      @__art_provs.unshift(art_prov)
     end
 
     wx_delete = instance_method(:delete)
