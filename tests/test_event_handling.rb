ca5a17c530a0119529ef1b0723b9bf87efc7c81f
Martin Corino <mcorino@m2c-software.nl>
2023-06-26 14:45:32 +0200

    add EventFilter tests

--- tests/test_event_handling.rb
+++ tests/test_event_handling.rb
@@ -89,4 +89,68 @@ class EventHandlingTests < Test::Unit::TestCase
     assert_boolean(win.test_cmd_event)
     assert_boolean(!win.child.test_cmd_event)
   end
+
+  class MyEventFilter < Wx::EventFilter
+    def initialize
+      super
+      @filtered = false
+    end
+
+    attr_accessor :filtered
+
+    def filter_event(event)
+      @filtered = true if event.is_a?(TestEvent)
+      Wx::EventFilter::Event_Skip
+    end
+  end
+
+  def test_event_filter
+    filter = MyEventFilter.new
+    assert_nothing_raised { Wx::EvtHandler.add_filter(filter) }
+    GC.start
+    win = TestFrame.new
+    win.child.event_handler.process_event(TestEvent.new)
+    GC.start
+    assert(filter.filtered)
+    filter.filtered = false
+    win.child.event_handler.process_event(TestCmdEvent.new)
+    GC.start
+    assert(!filter.filtered)
+    assert_nothing_raised { Wx::EvtHandler.remove_filter(filter) }
+    GC.start
+    win.child.event_handler.process_event(TestEvent.new)
+    assert(!filter.filtered)
+    GC.start
+  end
+
+  class MyEventFilter2 < Wx::EventFilter
+
+    class << self
+      attr_accessor :filtered
+    end
+
+    def filter_event(event)
+      MyEventFilter2.filtered = true if event.is_a?(TestEvent)
+      Wx::EventFilter::Event_Skip
+    end
+  end
+
+  def test_event_filter_clear
+    assert_nothing_raised { Wx::EvtHandler.add_filter(MyEventFilter2.new) }
+    GC.start
+    win = TestFrame.new
+    win.child.event_handler.process_event(TestEvent.new)
+    GC.start
+    assert(MyEventFilter2.filtered)
+    MyEventFilter2.filtered = false
+    win.child.event_handler.process_event(TestCmdEvent.new)
+    GC.start
+    assert(!MyEventFilter2.filtered)
+    assert_nothing_raised { Wx::EvtHandler.clear_filters }
+    GC.start
+    win.child.event_handler.process_event(TestEvent.new)
+    assert(!MyEventFilter2.filtered)
+    GC.start
+  end
+
 end
