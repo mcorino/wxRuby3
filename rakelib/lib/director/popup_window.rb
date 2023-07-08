651cf8a3f35fa21f3bfdbeb87e406553e99d7cc7
Martin Corino <martin@corino.nl>
2023-07-08 18:01:40 +0200

    fix wxPopupWindow for OSX (also improves other ports)

--- rakelib/lib/director/popup_window.rb
+++ rakelib/lib/director/popup_window.rb
@@ -14,6 +14,24 @@ module WXRuby3
       def setup
         spec.items << 'wxPopupTransientWindow'
         super
+        spec.items.each do |itm|
+          spec.no_proxy("#{itm}::ClearBackground",
+                        "#{itm}::Enable",
+                        "#{itm}::GetHelpTextAtPoint",
+                        "#{itm}::GetMaxSize",
+                        "#{itm}::GetMinSize",
+                        "#{itm}::Refresh",
+                        "#{itm}::Update")
+        end
+        # add these to the generated interface to be parsed by SWIG
+        # the wxWidgets docs are flawed in this respect that several reimplemented
+        # virtual methods are not documented at the reimplementing class as such
+        # that would cause them missing from the interface which would cause a problem
+        # for a SWIG director redirecting to the Ruby class as the SWIG wrappers
+        # redirect explicitly to the implementation at the same class level as the wrapper
+        # for upcalls
+        spec.extend_interface('wxPopupWindow',
+                              'virtual bool Show(bool show = true) override')
       end
 
     end # class PopupWindow
