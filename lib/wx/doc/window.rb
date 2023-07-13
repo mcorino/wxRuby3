78a6075fea947b8c21f490dc7eb985d4dc60bffe
Martin Corino <mcorino@m2c-software.nl>
2023-07-13 21:29:57 +0200

    add #find_window_by_xxx docs

--- lib/wx/doc/window.rb
+++ lib/wx/doc/window.rb
@@ -19,4 +19,27 @@ class Wx::Window
   # @param [Proc] block
   def locked(&block); end
 
+  # Find the first child window with the given id recursively in the window hierarchy of this window.
+  #
+  # Window with the given id or nil if not found.
+  # @see Wx::Window.find_window_by_id
+  # @param id [Integer]
+  # @return [Wx::Window]
+  def find_window_by_id(id) end
+
+  # Find the first child window with the given label recursively in the window hierarchy of this window.
+  #
+  # Window with the given label or nil if not found.
+  # @see Wx::Window.find_window_by_label
+  # @param label [String]
+  # @return [Wx::Window]
+  def find_window_by_label(label) end
+
+  # Find the first child window with the given name (as given in a window constructor or {Wx::Window#create} function call) recursively in the window hierarchy of this window.
+  #
+  # Window with the given name or nil if not found.
+  # @param name [String]
+  # @return [Wx::Window]
+  def find_window_by_name(name) end
+
 end
