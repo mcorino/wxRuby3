23af3375d86e28facbcd2856a918aa5688d14d6a
Martin Corino <mcorino@m2c-software.nl>
2023-07-25 11:24:25 +0200

    fix TextCompleterSimple#get_completions

--- rakelib/lib/director/text_entry.rb
+++ rakelib/lib/director/text_entry.rb
@@ -23,6 +23,31 @@ module WXRuby3
         spec.add_header_code '#define wxTextEntry wxTextEntryBase'
         spec.disown 'wxTextCompleter *completer' # managed by wxWidgets after passing in
         spec.map_apply 'long * OUTPUT' => 'long *' # for GetSelection
+        # for wxTextCompleterSimple::GetCompletions
+        spec.map 'wxArrayString &res' => 'Array<String>' do
+
+          map_in ignore: true, temp: 'wxArrayString tmp', code: '$1 = &tmp;'
+
+          map_argout code: <<~__CODE
+            $result = rb_ary_new();
+            for (size_t i = 0; i < $1->GetCount(); i++)
+            {
+              rb_ary_push($result,WXSTR_TO_RSTR( $1->Item(i) ) );
+            }
+          __CODE
+
+          map_directorargout code: <<~__CODE
+            if (result != Qnil && TYPE(result) == T_ARRAY)
+            {
+              for (int i = 0, n = RARRAY_LEN(result); i < n ;i++)
+              {
+                VALUE rb_comp = rb_ary_entry(result, i);
+                wxString comp = RSTR_TO_WXSTR(rb_comp);
+                $1.Add(comp);
+              }
+            }
+          __CODE
+        end
       end
     end # class TextEntry
 
