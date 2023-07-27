8b0f2bf1086e666f09d2d256341e9c997128b002
Martin Corino <mcorino@m2c-software.nl>
2023-07-27 14:06:01 +0200

    improve identifier transforms

--- rakelib/lib/generate/doc.rb
+++ rakelib/lib/generate/doc.rb
@@ -277,7 +277,7 @@ module WXRuby3
 
       def _ident_str_to_doc(s, ref_scope = nil)
         return s if no_idents?
-        return s if s.start_with?('wxRuby')
+        return s if %w[wxRuby wxMSW wxOSX wxGTK wxX11].any? { |w| s.start_with?(w) }
         nmlist = s.split('::')
         nm_str = nmlist.shift.to_s
         constnm = rb_wx_name(nm_str)
