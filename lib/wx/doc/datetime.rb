74c531f1a1d5e0ad9334fd602b6712311d3251f9
Martin Corino <mcorino@m2c-software.nl>
2023-08-30 12:35:09 +0200

    provide orginal DefaultDateTime constant

--- lib/wx/doc/datetime.rb
+++ lib/wx/doc/datetime.rb
@@ -4,4 +4,7 @@ module Wx
   # Default/Null time (mostly equates to `now`).
   DEFAULT_DATE_TIME = nil
 
+  # Default/Null time (mostly equates to `now`).
+  DefaultDateTime = DEFAULT_DATE_TIME
+
 end
