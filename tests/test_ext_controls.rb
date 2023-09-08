5075d68a754fe74f401a8a30815570ce4fbaa128
Martin Corino <mcorino@m2c-software.nl>
2023-09-08 13:29:51 +0200

    add Wx::RearrangeCtrl and test

--- tests/test_ext_controls.rb
+++ tests/test_ext_controls.rb
@@ -274,6 +274,7 @@ class RearrangeListTests < WxRuby::Test::GUITests
   def test_rearrange_list
     assert_equal([~1, ~2, 0], list.get_current_order)
     list.set_focus
+    Wx.get_app.yield
     assert_true(list.move_current_down)
     assert_equal([~2, ~1, 0], list.get_current_order)
   end
@@ -294,9 +295,10 @@ class RearrangeCtrlTests < WxRuby::Test::GUITests
 
   attr_reader :ra_ctrl
 
-  def test_rearrange_list
+  def test_rearrange_ctrl
     assert_equal([~1, ~2, 0], ra_ctrl.list.get_current_order)
     ra_ctrl.set_focus
+    Wx.get_app.yield
     assert_true(ra_ctrl.list.move_current_down)
     assert_equal([~2, ~1, 0], ra_ctrl.list.get_current_order)
   end
