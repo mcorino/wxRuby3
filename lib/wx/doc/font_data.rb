cef02dac4b56bc336e55b708b1d86aef3642bd0e
Martin Corino <mcorino@m2c-software.nl>
2023-09-29 11:41:41 +0200

    add constants missing from interface docs

---
+++ lib/wx/doc/font_data.rb
@@ -0,0 +1,16 @@
+# :stopdoc:
+# Copyright (c) 2023 M.J.N. Corino, The Netherlands
+#
+# This software is released under the MIT license.
+# :startdoc:
+
+
+module Wx
+
+  FONTRESTRICT_NONE = 0
+
+  FONTRESTRICT_SCALABLE = 1 << 0
+
+  FONTRESTRICT_FIXEDPITCH = 1 << 1
+
+end
