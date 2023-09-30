f6dd6a0ec34741f71ecb20226edc966da82e81a2
Martin Corino <mcorino@m2c-software.nl>
2023-09-30 19:21:28 +0200

    fix wxTreeItemId mappings

--- rakelib/lib/typemap/tree_itemid.rb
+++ rakelib/lib/typemap/tree_itemid.rb
@@ -26,21 +26,43 @@ module WXRuby3
             // implemented in TreeEvent.cpp
             extern VALUE _wxRuby_Wrap_wxTreeItemId(const wxTreeItemId& id);
             extern wxTreeItemId _wxRuby_Unwrap_wxTreeItemId(VALUE id);
+            extern bool _wxRuby_Is_wxTreeItemId(VALUE id);
             __CODE
 
           map_in temp: 'wxTreeItemId tmpId', code: <<~__CODE
             if ($input != Qnil) tmpId = _wxRuby_Unwrap_wxTreeItemId($input);
             $1 = &tmpId;
             __CODE
+
           map_directorin code: '$input = _wxRuby_Wrap_wxTreeItemId($1);'
+
+          map_typecheck precedence: 'POINTER', code: '$1 = _wxRuby_Is_wxTreeItemId($input);'
         end
 
         map 'wxTreeItemId' => 'Wx::TreeItemId' do
+
+          add_header_code <<~__CODE
+            // implemented in TreeEvent.cpp
+            extern VALUE _wxRuby_Wrap_wxTreeItemId(const wxTreeItemId& id);
+            extern wxTreeItemId _wxRuby_Unwrap_wxTreeItemId(VALUE id);
+            extern bool _wxRuby_Is_wxTreeItemId(VALUE id);
+            __CODE
+
+          map_in temp: 'wxTreeItemId tmpId', code: <<~__CODE
+            if ($input != Qnil) tmpId = _wxRuby_Unwrap_wxTreeItemId($input);
+            $1 = &tmpId;
+            __CODE
+
+          map_directorin code: '$input = _wxRuby_Wrap_wxTreeItemId($1);'
+
           map_out code: '$result = _wxRuby_Wrap_wxTreeItemId($1);'
+
           map_directorout temp: 'wxTreeItemId tmpId', code: <<~__CODE
             if ($input != Qnil) tmpId = _wxRuby_Unwrap_wxTreeItemId($input);
             $result = &tmpId;
           __CODE
+
+          map_typecheck precedence: 'POINTER', code: '$1 = _wxRuby_Is_wxTreeItemId($input);'
         end
 
       end # define
