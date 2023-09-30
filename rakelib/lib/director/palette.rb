4e9fc3d2373e5b4487b4f93ee155859d1c42ce96
Martin Corino <mcorino@m2c-software.nl>
2023-09-30 17:29:18 +0200

    fix unsigned char * in and inout arg mappings

--- rakelib/lib/director/palette.rb
+++ rakelib/lib/director/palette.rb
@@ -16,6 +16,55 @@ module WXRuby3
         super
         spec.disable_proxies
         spec.gc_as_untracked 'wxPalette'
+        spec.ignore 'wxPalette::GetRGB(int,unsigned char *,unsigned char *,unsigned char *) const'
+        spec.extend_interface 'wxPalette', 'bool GetRGB(int pixel,unsigned char *red_out,unsigned char *green_out,unsigned char *blue_out) const'
+        # for GetRGB
+        spec.map 'unsigned char *red_out', 'unsigned char *green_out', 'unsigned char *blue_out', as: 'Integer' do
+          map_in ignore: true, temp: 'unsigned char cv', code: '$1 = &cv;'
+          map_argout code: '$result = SWIG_Ruby_AppendOutput($result, UINT2NUM(cv$argnum));'
+        end
+        # for ctor and Create
+        spec.map 'int n, const unsigned char *red' => 'Array<Integer>' do
+          map_in temp: 'std::unique_ptr<unsigned char[]> arr, int _global_len', code: <<~__CODE
+            if (TYPE($input) == T_ARRAY)
+            {
+              _global_len = $1 = RARRAY_LEN($input);
+              arr = std::make_unique<unsigned char[]>($1);
+              $2 = arr.get ();
+              for (int i=0; i<$1 ;++i)
+              {
+                $2[i] = NUM2UINT(rb_ary_entry($input, i));
+              }
+            }
+            else
+            {
+              rb_raise(rb_eArgError, "Expected an array of integers for %d", $argnum-1);
+            }
+          __CODE
+          map_typecheck precedence: 'POINTER', code: '$1 = (TYPE($input) == T_ARRAY);'
+        end
+        spec.map 'const unsigned char *green', 'const unsigned char *blue', as: 'Array<Integer>' do
+          map_in temp: 'std::unique_ptr<unsigned char[]> arr', code: <<~__CODE
+            if (TYPE($input) == T_ARRAY && _global_len == RARRAY_LEN($input))
+            {
+              int len = RARRAY_LEN($input);
+              arr = std::make_unique<unsigned char[]>(len);
+              $1 = arr.get ();
+              for (int i=0; i<len ;++i)
+              {
+                $1[i] = NUM2UINT(rb_ary_entry($input, i));
+              }
+            }
+            else
+            {
+              if (TYPE($input) == T_ARRAY)
+                rb_raise(rb_eArgError, "Expected an array of integers of size %d for %d", _global_len, $argnum-1);
+              else
+                rb_raise(rb_eArgError, "Expected an array of integers for %d", $argnum-1);
+            }
+          __CODE
+          map_typecheck precedence: 'POINTER', code: '$1 = (TYPE($input) == T_ARRAY);'
+        end
         if Config.instance.wx_port == :wxQT
           # mismatched implementation which does nothing anyway
           spec.ignore 'wxPalette::wxPalette(int, const unsigned char *, const unsigned char *, const unsigned char *)'
