dbf778317c51ae5774c56d45e01f7b1442e15fcf
Martin Corino <mcorino@m2c-software.nl>
2023-10-05 14:07:12 +0200

    fix 'onlyfor' handling

--- rakelib/lib/director/textctrl.rb
+++ rakelib/lib/director/textctrl.rb
@@ -20,11 +20,11 @@ module WXRuby3
         spec.include_mixin 'wxTextCtrl', { 'Wx::TextEntry' => 'wxTextEntryBase' }
         spec.override_inheritance_chain('wxTextCtrl', %w[wxControl wxWindow wxEvtHandler wxObject])
         spec.ignore 'wxTextCtrl::HitTest(const wxPoint &,long *)'
-        if Config.instance.wx_port == :wxGTK && Config.instance.wx_version >= '3.3.0'
+        if Config.instance.wx_port == :wxgtk && Config.instance.wx_version >= '3.3.0'
           spec.ignore 'wxTextCtrl::GTKGetTextBuffer',
                       'wxTextCtrl::GTKGetEditable'
         end
-        if Config.instance.wx_port == :wxQT
+        if Config.instance.wx_port == :wxqt
           # not implemented
           spec.ignore 'wxTextCtrl::OnDropFiles'
         end
--- rakelib/lib/extractor.rb
+++ rakelib/lib/extractor.rb
@@ -220,8 +220,8 @@ module WXRuby3
           @brief_doc = bd # Should be just one <para> element
           @detailed_doc = element.xpath('detaileddescription')
           if (el = @detailed_doc.at_xpath('para/onlyfor'))
-            @only_for = el.text.strip.split(',').collect { |s| "__#{s.upcase}__" }
-            @ignored = @only_for.none? { |s| Config.instance.features_set?(s) }
+            @only_for = el.text.strip.split(',').collect { |s| s.downcase }
+            @ignored = @only_for.none? { |s| Config.instance.wx_port == s.to_sym }
             @docs_ignored = @ignored
           end
         end
