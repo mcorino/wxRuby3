5092b90a66a2a6ad600909a70726ad5e2664125f
Martin Corino <mcorino@m2c-software.nl>
2023-10-14 13:51:32 +0200

    extend RichTextCtrl tests

--- tests/test_richtext.rb
+++ tests/test_richtext.rb
@@ -67,11 +67,69 @@ class RichTextCtrlWriteTests < WxRuby::Test::GUITests
     assert_equal('Second Line', richtext.get_line_text(1))
   end
 
-end
+  def test_write_image
+    assert_nothing_raised { richtext.write_image(Wx::ArtLocator.find_art(:wxruby, art_type: :bitmap, art_section: 'test_art', bmp_type: Wx::BitmapType::BITMAP_TYPE_PNG), Wx::BitmapType::BITMAP_TYPE_PNG) }
+    img_obj = richtext.buffer.get_leaf_object_at_position(0)
+    assert_kind_of(Wx::RTC::RichTextImage, img_obj)
+  end
 
-# the timing of the RichTextCtrl update is too unreliable
-# to use this in CI builds
-unless ::Test::Unit::TestCase.is_ci_build?
+  def test_write_text_box
+    attr1 = Wx::RichTextAttr.new
+    attr1.get_text_box_attr.margins.left.set_value(20, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.margins.top.set_value(20, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.margins.right.set_value(20, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.margins.bottom.set_value(20, Wx::TEXT_ATTR_UNITS_PIXELS)
+
+    attr1.get_text_box_attr.border.set_colour(:BLACK)
+    attr1.get_text_box_attr.border.set_width(1, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.border.set_style(Wx::TEXT_BOX_ATTR_BORDER_SOLID)
+
+    textBox = richtext.write_text_box(attr1)
+    richtext.set_focus_object(textBox)
+
+    richtext.write_text("This is a text box. Just testing! Once more unto the breach, dear friends, once more...")
+
+    richtext.set_focus_object(nil) # Set the focus back to the main buffer
+    txt_box = richtext.buffer.get_leaf_object_at_position(0)
+    assert_kind_of(Wx::RTC::RichTextBox, txt_box)
+  end
+  
+  def test_write_table
+    attr1 = Wx::RichTextAttr.new
+    attr1.get_text_box_attr.margins.left.set_value(5, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.margins.top.set_value(5, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.margins.right.set_value(5, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.margins.bottom.set_value(5, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.padding.apply(richtext.get_default_style_ex.get_text_box_attr.margins)
+
+    attr1.get_text_box_attr.border.set_colour(:BLACK)
+    attr1.get_text_box_attr.border.set_width(1, Wx::TEXT_ATTR_UNITS_PIXELS)
+    attr1.get_text_box_attr.border.set_style(Wx::TEXT_BOX_ATTR_BORDER_SOLID)
+
+    cellAttr = Wx::RichTextAttr.new(attr1)
+    cellAttr.get_text_box_attr.width.set_value(200, Wx::TEXT_ATTR_UNITS_PIXELS)
+    cellAttr.get_text_box_attr.height.set_value(150, Wx::TEXT_ATTR_UNITS_PIXELS)
+
+    table = richtext.write_table(6, 4, attr1, cellAttr)
+
+    assert_kind_of(Wx::RTC::RichTextTable, table)
+    assert_equal(6, table.row_count)
+    assert_equal(4, table.column_count)
+
+    table.get_row_count.times do |j|
+      table.get_column_count.times do |i|
+        msg = "This is cell %d, %d" % [(j+1), (i+1)]
+        richtext.set_focus_object(table.cell(j, i))
+        richtext.write_text(msg)
+      end
+    end
+
+    cell = table.get_cell(0, 0)
+    assert_kind_of(Wx::RTC::RichTextCell, cell)
+    assert_equal('This is cell 1, 1', cell.get_text)
+  end
+
+end
 
 class RichTextCtrlFieldTypeTests < WxRuby::Test::GUITests
 
@@ -136,9 +194,11 @@ class RichTextCtrlFieldTypeTests < WxRuby::Test::GUITests
   def test_write_custom_field
     rt_field = richtext.write_field('TEST', Wx::RTC::RichTextProperties.new)
     assert_kind_of(Wx::RTC::RichTextField, rt_field)
-    assert_true(check_is_drawn)
+    # the timing of the RichTextCtrl update is too unreliable
+    # to use this in CI builds
+    unless is_ci_build?
+      assert_true(check_is_drawn)
+    end
   end
-
-end
 
 end
