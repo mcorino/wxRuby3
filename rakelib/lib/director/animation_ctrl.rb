919cc3c4953e01f6dd02cb39ea6bb22d5b9eb3b0
Martin Corino <mcorino@m2c-software.nl>
2023-10-20 16:11:28 +0200

    add Wx::AnimationBundle support for wxw >= 3.3.0

--- rakelib/lib/director/animation_ctrl.rb
+++ rakelib/lib/director/animation_ctrl.rb
@@ -18,6 +18,26 @@ module WXRuby3
 
       def setup
         super
+        if Config.instance.wx_version >= '3.3.0'
+          spec.items << 'wxAnimationBundle'
+          spec.ignore 'wxAnimationBundle::GetAll', ignore_doc: false
+          spec.add_extend_code 'wxAnimationBundle', <<~__HEREDOC
+            VALUE get_all() const
+            {
+              const std::vector<wxAnimation>& ani_list = $self->GetAll();
+              VALUE rb_ani_list = rb_ary_new();
+              for (const wxAnimation& ani : ani_list)
+              {
+                VALUE rb_ani = SWIG_NewPointerObj(new wxAnimation(ani), SWIGTYPE_p_wxAnimation, SWIG_POINTER_OWN);
+                rb_ary_push(rb_ani_list, rb_ani);
+              }
+              return rb_ani_list;
+            }
+            __HEREDOC
+          spec.map 'const std::vector<wxAnimation>&' => 'Array<Wx::Animation>', swig: false do
+            map_out code: ''
+          end
+        end
         spec.do_not_generate :variables, :enums, :defines, :functions
       end
     end # class AnimationCtrl
