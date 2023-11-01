5f5d74b841a7e9f58b8bf0500cecd5341996564e
Martin Corino <mcorino@m2c-software.nl>
2023-11-01 15:29:39 +0100

    for WXMSW only

--- lib/wx/core/task_bar_button.rb
+++ lib/wx/core/task_bar_button.rb
@@ -5,14 +5,15 @@
 
 module Wx
 
-  class TaskBarButton
+  if Wx::PLATFORM == 'WXMSW'
+    class TaskBarButton
 
-    wx_remove_thumb_bar_button = instance_method :remove_thumb_bar_button
-    define_method :remove_thumb_bar_button do |button|
-      button = button.get_id if button.is_a?(Wx::ThumbBarButton)
-      wx_remove_thumb_bar_button.bind(self).call(button)
+      wx_remove_thumb_bar_button = instance_method :remove_thumb_bar_button
+      define_method :remove_thumb_bar_button do |button|
+        button = button.get_id if button.is_a?(Wx::ThumbBarButton)
+        wx_remove_thumb_bar_button.bind(self).call(button)
+      end
+
     end
-
   end
-
 end
