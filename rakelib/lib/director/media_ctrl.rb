c2ce8d6d9daabca4dfd45201ea1045bf93cbf4a9
Martin Corino <martin@corino.nl>
2023-11-01 15:16:34 +0100

    add type mapping for Ruby URI

--- rakelib/lib/director/media_ctrl.rb
+++ rakelib/lib/director/media_ctrl.rb
@@ -16,6 +16,34 @@ module WXRuby3
 
       def setup
         super
+        spec.map 'const wxURI&' => 'URI' do
+          add_header <<~__CODE
+            static VALUE wxRuby_GetRubyURIClass()
+            {
+              static VALUE rb_cURI = Qnil;
+              if (NIL_P(rb_cURI))
+              {
+                rb_require("uri");
+                rb_cURI = rb_const_get(rb_cObject, rb_intern("URI"));
+              }
+              return rb_cURI;
+            }
+            static WxRuby_ID to_s_id("to_s");
+            __CODE
+          map_in temp: 'wxURI tmp', code: <<~__CODE
+            if (rb_obj_is_kind_of($input, wxRuby_GetRubyURIClass()))
+            {
+              VALUE s = rb_funcall($input, to_s_id(), 0);
+              tmp = wxURI(RSTR_TO_WXSTR(s));
+              $1 = &tmp;
+            }
+            else
+            {
+              rb_raise(rb_eArgError, "expected URI for %d", $argnum-1);
+            }                        
+            __CODE
+          map_typecheck precedence: 'POINTER', code: '$1 = rb_obj_is_kind_of($input, wxRuby_GetRubyURIClass());'
+        end
         spec.do_not_generate :variables, :enums, :defines, :functions
       end
     end # class MediaCtrl
