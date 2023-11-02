276c6aedebc38866be25e38692f633574eaf1a14
Martin Corino <mcorino@m2c-software.nl>
2023-11-02 14:22:22 +0100

    update mediactrl test so it also works for WXGTK

--- tests/test_media_ctrl.rb
+++ tests/test_media_ctrl.rb
@@ -5,7 +5,7 @@
 require_relative './lib/wxframe_runner'
 
 # test seems to deadlock for WXGTK
-if Wx.has_feature?(:USE_MEDIACTRL) && Wx::PLATFORM != 'WXGTK'
+if Wx.has_feature?(:USE_MEDIACTRL)
 
 require 'uri'
 
@@ -25,10 +25,12 @@ class MediaCtrlTests < WxRuby::Test::GUITests
 
   def test_uri
     # just checking if Ruby URI is properly mapped
-    uri = URI('/path/to/somewhere/media.type')
-    # can't check for true/false since the return value doesn't seem particularly
-    # reliable for non-existing media resources
-    assert_boolean(media.load(uri))
+    uri = URI("file://#{File.join(__dir__, 'media/beep_lo.wav')}")
+    assert_true(media.load(uri))
+    # it doesn't seem the mediactrl returns false if the resource is not a media file
+    # as long as it is an existing file (it seems Windows/MacOS even return true if the resource is non-existent)
+    uri = URI("file://#{File.join(__dir__, 'art/test_art/image/wxruby.png')}")
+    assert_true(media.load(uri))
   end
 
 end
