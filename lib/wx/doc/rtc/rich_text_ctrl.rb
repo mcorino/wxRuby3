7356ca2b56f9113d359f084cccb82c292658398b
Martin Corino <mcorino@m2c-software.nl>
2023-11-05 14:05:37 +0100

    add #each_line doc

---
+++ lib/wx/doc/rtc/rich_text_ctrl.rb
@@ -0,0 +1,25 @@
+# :stopdoc:
+# Copyright (c) 2023 M.J.N. Corino, The Netherlands
+#
+# This software is released under the MIT license.
+# :startdoc:
+
+
+module Wx
+
+  module RTC
+
+    class RichTextCtrl
+
+      # Yield each line to the given block.
+      # Returns an Enumerator if no block given.
+      # @yieldparam [String] line the line yielded
+      # @yieldparam [Integer] line_nr the line nr
+      # @return [Object,Enumerator] last result of block or Enumerator if no block given.
+      def each_line; end
+
+    end
+
+  end
+
+end
