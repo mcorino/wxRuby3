d57e02a4d69db00afb8c2a2a22446d7ce15d221f
Martin Corino <mcorino@m2c-software.nl>
2023-11-06 16:18:42 +0100

    allow more results

--- tests/test_media_ctrl.rb
+++ tests/test_media_ctrl.rb
@@ -25,12 +25,12 @@ class MediaCtrlTests < WxRuby::Test::GUITests
 
   def test_uri
     # just checking if Ruby URI is properly mapped
+    # it doesn't seem the mediactrl returns a very consistent result whether the resource is not a media file
+    # or if it is a non-existing file so only check whether it is a boolean return.
     uri = URI("file://#{File.join(__dir__, 'media/beep_lo.wav')}")
-    assert_true(media.load(uri))
-    # it doesn't seem the mediactrl returns false if the resource is not a media file
-    # as long as it is an existing file (it seems Windows/MacOS even return true if the resource is non-existent)
+    assert_boolean(media.load(uri))
     uri = URI("file://#{File.join(__dir__, 'art/test_art/image/wxruby.png')}")
-    assert_true(media.load(uri))
+    assert_boolean(media.load(uri))
   end
 
 end
