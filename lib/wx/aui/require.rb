aded118577f61e94cb93f9f7ae8eaec6f8c4f94a
Martin Corino <mcorino@m2c-software.nl>
2023-11-06 12:34:26 +0100

    add missing require

--- lib/wx/aui/require.rb
+++ lib/wx/aui/require.rb
@@ -9,5 +9,6 @@ require_relative './events/evt_list'
 require_relative './auimanager'
 require_relative './auinotebook'
 require_relative './auifloatframe'
+require_relative './aui_tab_ctrl'
 
 Wx::Dialog.setup_dialog_functors(Wx::AUI)
