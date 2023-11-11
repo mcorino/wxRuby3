10823a42a70a8071bfe68b552f51a44cbcb5ecc5
Martin Corino <mcorino@m2c-software.nl>
2023-11-11 17:26:56 +0100

    add wxWidgets version to html doc

--- rakelib/yard/templates/default/fulldoc/html/setup.rb
+++ rakelib/yard/templates/default/fulldoc/html/setup.rb
@@ -26,5 +26,14 @@ end
 
 def logo_and_version
   wxver = Registry['Wx::WXRUBY_VERSION']
-  %Q{<div class='wxrb-logo'><img src='assets/logo.png' height='38'/><span class='wxrb-name'><a href="https://github.com/mcorino/wxRuby3">wxRuby3</a> </span><span class='wxrb-version'>Version: #{::Kernel.eval(wxver.value)}</span></div>}
+  wxwver = Registry['Wx::WXWIDGETS_VERSION']
+  <<~__HTML
+  <div class='wxrb-logo'>
+    <img src='assets/logo.png' height='38'/>
+    <table><tbody>
+      <tr><td><span class='wxrb-name'><a href="https://github.com/mcorino/wxRuby3">wxRuby3</a></span></td><td><span class='wxrb-version'>Version: #{::Kernel.eval(wxver.value)}</span></td></tr>
+      <tr><td></td><td><span class="wxrb-wxver">(wxWidgets: #{::Kernel.eval(wxwver.value)})</span></td></tr>
+    </tbody></table>
+  </div>
+  __HTML
 end
