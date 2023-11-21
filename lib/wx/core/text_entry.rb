c07f9b2e5296c3f26aeb60a9791741f9818e77e9
Martin Corino <mcorino@m2c-software.nl>
2023-11-21 14:31:47 +0100

    fix missing overload check

--- lib/wx/core/text_entry.rb
+++ lib/wx/core/text_entry.rb
@@ -13,7 +13,7 @@ module Wx
     wx_auto_complete = instance_method :auto_complete
     define_method :auto_complete do |completer|
       if wx_auto_complete.bind(self).call(completer)
-        @completer = completer # keep the Ruby object alive
+        @completer = completer if completer.is_a?(Wx::TextCompleter) # keep the Ruby object alive
       end
     end
 
