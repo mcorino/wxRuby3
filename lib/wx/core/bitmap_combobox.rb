7b2628f22ab1691d4940ac244da246feefd1e776
Martin Corino <mcorino@m2c-software.nl>
2023-11-24 15:28:05 +0100

    fix problems with ignored inherited overloads

--- lib/wx/core/bitmap_combobox.rb
+++ lib/wx/core/bitmap_combobox.rb
@@ -8,32 +8,47 @@
 
 module Wx
 
-  class BitmapCombobox < Wx::ComboBox
+  class BitmapComboBox < Wx::ComboBox
 
     # We need to cache client data in Ruby variables as we cannot access items
     # during the GC mark phase as for some platforms (WXMSW at least) that would
     # involve calling methods that would break in that phase.
 
     wx_append = instance_method :append
-    define_method :append do |item, bitmap=Wx::NULL_BITMAP, data=nil|
-      itm_pos = if data
-                  wx_append.bind(self).call(item, bitmap, data)
-                else
-                  wx_append.bind(self).call(item, bitmap)
-                end
-      client_data_store.insert(itm_pos, data)
-      itm_pos
+    define_method :append do |item, *rest| #bitmap=Wx::NULL_BITMAP, data=nil|
+      if ::Array === item
+        super(item, *rest)
+      elsif rest.empty? || Wx::Bitmap === rest.first
+          bitmap, data = rest
+          bitmap ||= Wx::NULL_BITMAP
+          itm_pos = if data
+                      wx_append.bind(self).call(item, bitmap, data)
+                    else
+                      wx_append.bind(self).call(item, bitmap)
+                    end
+          client_data_store.insert(itm_pos, data)
+          itm_pos
+      else
+        super(item, *rest)
+      end
     end
 
     wx_insert = instance_method :insert
-    define_method :insert do |item, bitmap, pos, data=nil|
-      itm_pos = if data
-                  wx_insert.bind(self).call(item, bitmap, pos, data)
-                else
-                  wx_insert.bind(self).call(item, bitmap, pos)
-                end
-      client_data_store.insert(itm_pos, data)
-      itm_pos
+    define_method :insert do |item, *rest| # bitmap, pos, data=nil|
+      if ::Array === item
+        super(item, *rest)
+      elsif rest.empty? || Wx::Bitmap === rest.first
+        bitmap, pos, data = rest
+        itm_pos = if data
+                    wx_insert.bind(self).call(item, bitmap, pos, data)
+                  else
+                    wx_insert.bind(self).call(item, bitmap, pos)
+                  end
+        client_data_store.insert(itm_pos, data)
+        itm_pos
+      else
+        super(item, *rest)
+      end
     end
 
   end
