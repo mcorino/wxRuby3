05b3cf9dc17f28bc5b86242b7348a3a4352e19d2
Martin Corino <mcorino@m2c-software.nl>
2024-02-07 12:41:09 +0100

    update Cirrus CI; add distro setup tool scripts

---
+++ tools/scripts/setup-debian.sh
@@ -0,0 +1,1 @@
+sudo apt-get -q -o=Dpkg::Use-Pty=0 install -y git make gcc gpg xvfb xfonts-75dpi
---
+++ tools/scripts/setup-fedora.sh
@@ -0,0 +1,1 @@
+sudo dnf install -y git xorg-x11-server-Xvfb xorg-x11-fonts-75dpi which gcc make procps-ng
