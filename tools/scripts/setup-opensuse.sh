6790cc9beeeafd4d073a53da2964d60651a819e5
Martin Corino <mcorino@m2c-software.nl>
2024-02-07 15:44:49 +0100

    update distro setups

--- tools/scripts/setup-opensuse.sh
+++ tools/scripts/setup-opensuse.sh
@@ -1,1 +1,1 @@
-zypper install -y git xorg-x11-server-Xvfb xvfb-run xorg-x11-fonts curl gcc make
+zypper install -y git xorg-x11-server-Xvfb xvfb-run xorg-x11-fonts curl gcc make tar gzip
--- tools/scripts/setup-ubuntu.sh
+++ tools/scripts/setup-ubuntu.sh
@@ -1,2 +1,2 @@
 apt-get update
-apt-get install -y git make gcc gpg xvfb xfonts-75dpi curl
+apt-get install -y git make gcc gpg xvfb xfonts-75dpi curl procps
