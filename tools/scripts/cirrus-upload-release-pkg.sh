ab40dd82f33dbe9991ef42c794b5ba1a12b4ac4c
Martin Corino <mcorino@m2c-software.nl>
2024-02-09 11:17:31 +0100

    fix cirrus release upload script REST call

--- tools/scripts/cirrus-upload-release-pkg.sh
+++ tools/scripts/cirrus-upload-release-pkg.sh
@@ -18,10 +18,12 @@ for fpath in pkg/*.pkg
 do
   echo "Uploading $fpath..."
   name=$(basename "$fpath")
-  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
-  curl -X POST \
+  url_to_upload="https://uploads.github.com/repos/mcorino/wxruby3/releases/$CIRRUS_RELEASE/assets?name=$name"
+  curl -L -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "Content-Type: $file_content_type" \
+    $url_to_upload \
     --data-binary @$fpath \
-    --header "Authorization: token $GITHUB_TOKEN" \
-    --header "Content-Type: $file_content_type" \
-    $url_to_upload
 done
