8143e2704e7c650dcc5df340631b799f928bd49c
Martin Corino <mcorino@m2c-software.nl>
2024-02-11 14:37:02 +0100

    trying to fix platform setup

--- tools/scripts/cirrus/setup-opensuse-system-ruby.sh
+++ tools/scripts/cirrus/setup-opensuse-system-ruby.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "remove" ]; then
-  zypper remove -y ruby ruby-devel ruby2.5-rubygem-bundler libopenssl-1_1-devel
-  zypper install -y libopenssl-devel
+  zypper remove -y ruby ruby-devel ruby2.5-rubygem-bundler
+  zypper install -y libyaml-devel
 else
   zypper install -y ruby ruby-devel ruby2.5-rubygem-bundler
 fi
