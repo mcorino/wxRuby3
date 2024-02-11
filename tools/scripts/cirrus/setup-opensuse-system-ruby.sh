5a7f7e24cc075df9fcf6aa956932479e1914c6be
Martin Corino <mcorino@m2c-software.nl>
2024-02-11 12:31:32 +0100

    add another dependency

--- tools/scripts/cirrus/setup-opensuse-system-ruby.sh
+++ tools/scripts/cirrus/setup-opensuse-system-ruby.sh
@@ -3,5 +3,5 @@
 if [ "$1" == "remove" ]; then
   zypper remove -y ruby ruby-devel
 else
-  zypper install -y ruby ruby-devel ruby2.5-rubygem-bundler zlib-devel libopenssl-devel readline-devel
+  zypper install -y ruby ruby-devel ruby2.5-rubygem-bundler libffi-devel zlib-devel libopenssl-devel readline-devel
 fi
