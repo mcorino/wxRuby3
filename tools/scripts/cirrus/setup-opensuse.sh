de144037148387246495b75ff5c1dbe6106fbd73
Martin Corino <mcorino@m2c-software.nl>
2024-02-14 10:19:48 +0100

    workaround to install webkit2gtk without having to specify exact versions

--- tools/scripts/cirrus/setup-opensuse.sh
+++ tools/scripts/cirrus/setup-opensuse.sh
@@ -2,5 +2,5 @@
 
 zypper install -y git xorg-x11-server-Xvfb xvfb-run xorg-x11-fonts curl gcc make tar gzip
 if [ "$1" == "test" ]; then
-  zypper install -y gtk3 webkit2gtk4 gspell libnotify4 libsecret curl
+  zypper install -y gtk3 webkit2gtk3-devel gspell libnotify4 libsecret curl
 fi
