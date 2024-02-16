d68e2fa314cbbe5ffd7a763c7261adbe0b18300b
Martin Corino <mcorino@m2c-software.nl>
2024-02-16 17:04:50 +0100

    add Cirrus CI release task trigger

---
+++ tools/scripts/cirrus/start_release_tasks.rb
@@ -0,0 +1,40 @@
+
+require 'uri'
+require 'net/https'
+require 'json'
+
+$CIRRUS_TOKEN = ENV['CIRRUS_TOKEN'] || ''
+
+$cirrus_uri = URI('https://api.cirrus-ci.com/graphql')
+
+$query = %q[{ ownerRepository(platform: "github", owner: "mcorino", name: "wxRuby3") { id } }]
+
+$headers = {
+  'Authorization' => "Bearer #{$CIRRUS_TOKEN}",
+  'Content-Type' => 'application/json'
+}
+$response = Net::HTTP.post($cirrus_uri, { query:  $query }.to_json, $headers)
+
+if Net::HTTPOK === $response
+  $repository_id = JSON.parse($response.body)['data']['ownerRepository']['id']
+
+  $mutation = %Q[mutation StartReleaseBuild( $input: RepositoryCreateBuildInput! ) { createBuild(input: $input) { build { id } } }]
+  vars = {
+    input: {
+      repositoryId: "#{$repository_id}",
+      branch: "bin-packages",
+      clientMutationId: "wxRuby3"
+    }
+  }
+  $response = Net::HTTP.post($cirrus_uri, { query:  $mutation, variables: vars }.to_json, $headers)
+  if Net::HTTPOK === $response
+    $build_id = JSON.parse($response.body)['data']['createBuild']['build']['id']
+    puts "Started release build [#{$build_id}]"
+  else
+    $stderr.puts "Failed to start release build."
+    exit(1)
+  end
+else
+  $stderr.puts "Failed to retrieve repository id."
+  exit(1)
+end
