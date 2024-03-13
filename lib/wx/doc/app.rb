43124a82bceb5a2cf94b49df79908ebfdd68d7c1
Martin Corino <mcorino@m2c-software.nl>
2024-03-13 10:59:46 +0100

    fix, improve and extend macosx menu specials

--- lib/wx/doc/app.rb
+++ lib/wx/doc/app.rb
@@ -9,47 +9,79 @@ module Wx
 
   class App
 
-    # Set the menu item id for the About menu item.
-    # Default is {Wx::ID_ABOUT}
-    # @param [Integer] id
-    # @wxrb_require WXOSX
-    def set_mac_about_menu_itemid(id) end
-    alias :mac_about_menu_itemid= :set_mac_about_menu_itemid
+    class << self
 
-    # Get the current menu item id for the About menu item.
-    # Default is {Wx::ID_ABOUT}
-    # @return [Integer]
-    # @wxrb_require WXOSX
-    def get_mac_about_menu_itemid(id) end
-    alias :mac_about_menu_itemid :get_mac_about_menu_itemid
+      # Set the menu item id for the About menu item.
+      # Default is {Wx::ID_ABOUT}
+      # @param [Integer] id
+      # @wxrb_require WXOSX
+      def set_mac_about_menu_itemid(id) end
+      alias :mac_about_menu_itemid= :set_mac_about_menu_itemid
 
-    # Set the menu item id for the Preferences menu item.
-    # Default is {Wx::ID_PREFERENCES}
-    # @param [Integer] id
-    # @wxrb_require WXOSX
-    def set_mac_preferences_menu_itemid(id) end
-    alias :mac_preferences_menu_itemid= :set_mac_preferences_menu_itemid
+      # Get the current menu item id for the About menu item.
+      # Default is {Wx::ID_ABOUT}
+      # @return [Integer]
+      # @wxrb_require WXOSX
+      def get_mac_about_menu_itemid; end
+      alias :mac_about_menu_itemid :get_mac_about_menu_itemid
 
-    # Get the current menu item id for the Preferences menu item.
-    # Default is {Wx::ID_PREFERENCES}
-    # @return [Integer]
-    # @wxrb_require WXOSX
-    def get_mac_preferences_menu_itemid(id) end
-    alias :mac_preferences_menu_itemid :get_mac_preferences_menu_itemid
+      # Set the menu item id for the Preferences menu item.
+      # Default is {Wx::ID_PREFERENCES}
+      # @param [Integer] id
+      # @wxrb_require WXOSX
+      def set_mac_preferences_menu_itemid(id) end
+      alias :mac_preferences_menu_itemid= :set_mac_preferences_menu_itemid
 
-    # Set the menu item id for the Exit menu item.
-    # Default is {Wx::ID_EXIT}
-    # @param [Integer] id
-    # @wxrb_require WXOSX
-    def set_mac_exit_menu_itemid(id) end
-    alias :mac_exit_menu_itemid= :set_mac_exit_menu_itemid
+      # Get the current menu item id for the Preferences menu item.
+      # Default is {Wx::ID_PREFERENCES}
+      # @return [Integer]
+      # @wxrb_require WXOSX
+      def get_mac_preferences_menu_itemid; end
+      alias :mac_preferences_menu_itemid :get_mac_preferences_menu_itemid
 
-    # Get the current menu item id for the Exit menu item.
-    # Default is {Wx::ID_EXIT}
-    # @return [Integer]
-    # @wxrb_require WXOSX
-    def get_mac_exit_menu_itemid(id) end
-    alias :mac_exit_menu_itemid :get_mac_exit_menu_itemid
+      # Set the menu item id for the Exit menu item.
+      # Default is {Wx::ID_EXIT}
+      # @param [Integer] id
+      # @wxrb_require WXOSX
+      def set_mac_exit_menu_itemid(id) end
+      alias :mac_exit_menu_itemid= :set_mac_exit_menu_itemid
+
+      # Get the current menu item id for the Exit menu item.
+      # Default is {Wx::ID_EXIT}
+      # @return [Integer]
+      # @wxrb_require WXOSX
+      def get_mac_exit_menu_itemid; end
+      alias :mac_exit_menu_itemid :get_mac_exit_menu_itemid
+
+      # Set the menu title for the Help menu.
+      # Default is '&Help'
+      # @param [String] title
+      # @wxrb_require WXOSX
+      def set_mac_help_menu_title(title) end
+      alias :mac_help_menu_title= :set_mac_help_menu_title
+
+      # Get the current title for the Help menu.
+      # Default is '&Help'
+      # @return [String]
+      # @wxrb_require WXOSX
+      def get_mac_help_menu_title; end
+      alias :mac_help_menu_title :get_mac_help_menu_title
+
+      # Set the menu title for the Window menu.
+      # Default is '&Window'
+      # @param [String] title
+      # @wxrb_require WXOSX
+      def set_mac_window_menu_title(title) end
+      alias :mac_window_menu_title= :set_mac_window_menu_title
+
+      # Get the current title for the Window menu.
+      # Default is '&Window'
+      # @return [String]
+      # @wxrb_require WXOSX
+      def get_mac_window_menu_title; end
+      alias :mac_window_menu_title :get_mac_window_menu_title
+
+    end
 
   end
 
