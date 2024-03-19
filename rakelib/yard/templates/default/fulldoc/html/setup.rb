cf6d0a89e12a8fcb49bdbc12b61b6281b7d8aa20
Martin Corino <mcorino@m2c-software.nl>
2024-03-19 14:29:08 +0100

    fix yet another flaw in yardoc

--- rakelib/yard/templates/default/fulldoc/html/setup.rb
+++ rakelib/yard/templates/default/fulldoc/html/setup.rb
@@ -4,16 +4,16 @@ def init
   # It seems YARD messes things up so that a lot of classes, modules and constants are not properly
   # registered in their enclosing namespaces.
   # This hack makes sure that if that is the case we fix that here.
-  all_objects = Registry.all(:class, :constant, :module)
+  all_objects = Registry.all(:class, :constant, :module, :method)
   all_objects.each do |c|
     if (ns = c.namespace)
       unless ns.children.any? { |nsc| nsc.path == c.path }
-        ns.children << c # class/module/constant missing from child list of enclosing namespace -> add here
+        ns.children << c # class/module/constant/method missing from child list of enclosing namespace -> add here
       end
     end
     if (ns = Registry[c.namespace.path])
       unless ns.children.any? { |nsc| nsc.path == c.path }
-        ns.children << c # class/module/constant missing from child list of enclosing namespace -> add here
+        ns.children << c # class/module/constant/method missing from child list of enclosing namespace -> add here
       end
     end
   end
