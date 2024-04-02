e395f1c14c638757640b3c4878274f75dbb8b5bc
Martin Corino <mcorino@m2c-software.nl>
2024-04-02 10:02:21 +0200

    add xz package (for xzcat; needed by rvm)

--- tools/scripts/cirrus/setup-opensuse.sh
+++ tools/scripts/cirrus/setup-opensuse.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-zypper install -y git xorg-x11-server-Xvfb xvfb-run xorg-x11-fonts curl gcc make tar gzip
+zypper install -y git xorg-x11-server-Xvfb xvfb-run xorg-x11-fonts curl gcc make tar gzip xz
 if [ "$1" == "test" ]; then
   zypper install -y gtk3 webkit2gtk3-devel libGLU1 gspell libnotify4 libsecret curl
 fi
