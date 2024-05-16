ffaac43d55fb8b0eb50469526ef9b4815f20e6e7
Martin Corino <mcorino@m2c-software.nl>
2024-05-16 18:48:32 +0200

    support newer Ubuntu release

--- tools/scripts/cirrus/setup-ubuntu.sh
+++ tools/scripts/cirrus/setup-ubuntu.sh
@@ -3,5 +3,13 @@
 apt-get update
 apt-get install -y git make gcc gpg xvfb xfonts-75dpi curl procps
 if [ "$1" == "test" ]; then
-  apt-get install -y 'libgtk-3-[0-9]+' 'libwebkit2gtk-4.0-[0-9]+' 'libgspell-1-[0-9]+' libnotify4 'libsecret-1-[0-9]+' curl
+  source /etc/os-release
+  case $VERSION_ID in
+    24.04)
+      apt-get install -y 'libgtk-3-[0-9]+' 'libwebkit2gtk-4.1-[0-9]+' 'libgspell-1-[0-9]+' libnotify4 'libsecret-1-[0-9]+' curl
+      ;;
+    *)
+      apt-get install -y 'libgtk-3-[0-9]+' 'libwebkit2gtk-4.0-[0-9]+' 'libgspell-1-[0-9]+' libnotify4 'libsecret-1-[0-9]+' curl
+      ;;
+  esac
 fi
