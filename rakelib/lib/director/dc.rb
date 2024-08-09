421b7e5fc5e482c138a8307e131892dbf2525615
Martin Corino <mcorino@m2c-software.nl>
2024-08-09 11:13:06 +0200

    add wxReadOnlyDC + wxInfoDC support for wxw >= 3.3.0

--- rakelib/lib/director/dc.rb
+++ rakelib/lib/director/dc.rb
@@ -16,6 +16,11 @@ module WXRuby3
 
       def setup
         super
+        _readDC = 'wxDC'
+        if Config.instance.wx_version >= '3.3.0'
+          spec.items << 'wxReadOnlyDC' << 'wxInfoDC'
+          _readDC = 'wxReadOnlyDC'
+        end
         spec.items << 'wxFontMetrics'
         # it's not safe to track DC objects as these are often created on the stack in C++
         # before being passed to Ruby methods
@@ -29,22 +34,24 @@ module WXRuby3
                     'wxFontMetrics::externalLeading',
                     'wxFontMetrics::averageWidth'
         spec.ignore [
-          'wxDC::GetPartialTextExtents',
-          'wxDC::DrawLines(const wxPointList *,wxCoord,wxCoord)',
-          'wxDC::DrawPolygon(const wxPointList *,wxCoord,wxCoord,wxPolygonFillMode)',
-          'wxDC::DrawSpline(const wxPointList *)',
-          'wxDC::GetLogicalOrigin(wxCoord *,wxCoord *) const',
-          'wxDC::GetHandle'
+                      'wxDC::DrawLines(const wxPointList *,wxCoord,wxCoord)',
+                      'wxDC::DrawPolygon(const wxPointList *,wxCoord,wxCoord,wxPolygonFillMode)',
+                      'wxDC::DrawSpline(const wxPointList *)',
+                      'wxDC::GetHandle'
+                    ]
+        spec.ignore [
+          "#{_readDC}::GetPartialTextExtents",
+          "#{_readDC}::GetLogicalOrigin(wxCoord *,wxCoord *) const",
         ]
+        spec.rename_for_ruby({
+           'GetDimensions' => "#{_readDC}::GetSize(wxCoord *, wxCoord *) const",
+           'GetDimensionsMM' => "#{_readDC}::GetSizeMM(wxCoord *, wxCoord *) const",
+           'GetTextSize' => "#{_readDC}::GetTextExtent(const wxString &) const",
+           'GetMultiLineTextSize' => "#{_readDC}::GetMultiLineTextExtent(const wxString &) const"
+         })
         spec.disable_proxies
         spec.disown 'wxGraphicsContext *ctx'
-        spec.rename_for_ruby({
-          'GetDimensions' => 'wxDC::GetSize(wxCoord *, wxCoord *) const',
-          'GetDimensionsMM' => 'wxDC::GetSizeMM(wxCoord *, wxCoord *) const',
-          'GetTextSize' => 'wxDC::GetTextExtent(const wxString &) const',
-          'GetMultiLineTextSize' => 'wxDC::GetMultiLineTextExtent(const wxString &) const'
-        })
-        spec.add_extend_code 'wxDC', <<~__HEREDOC
+        spec.add_extend_code _readDC, <<~__HEREDOC
           // Needs to return input parameter with list of lengths
           VALUE get_partial_text_extents(VALUE text) {
             wxString str = wxString(StringValuePtr(text), wxConvUTF8);
@@ -61,6 +68,27 @@ module WXRuby3
         # for GetUserScale and GetLogicalScale
         spec.map_apply 'double * OUTPUT' => 'double *'
         spec.swig_import 'swig/classes/include/wxGDICommon.h'
+        if Config.instance.wx_version >= '3.3.0'
+          # add similar block-style creator as #draw_on methods
+          spec.add_extend_code 'wxInfoDC', <<~__HEREDOC
+              static VALUE inform_on(wxWindow* win)
+              {
+                if (!wxRuby_IsAppRunning()) 
+                  rb_raise(rb_eRuntimeError, "A running Wx::App is required.");
+                if (!win)
+                  rb_raise(rb_eRuntimeError, "A running valid Wx::Window is required for argument.");
+                VALUE rc = Qnil;
+                if (rb_block_given_p ())
+                {
+                  wxInfoDC info_dc(win);
+                  wxInfoDC* dc_ptr = &info_dc;
+                  VALUE rb_dc = SWIG_NewPointerObj(SWIG_as_voidptr(dc_ptr), SWIGTYPE_p_wxInfoDC, 0);
+                  rc = rb_yield(rb_dc);
+                }
+                return rc;
+              }
+            __HEREDOC
+        end
       end
     end # class DC
 
