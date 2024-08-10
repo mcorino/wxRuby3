3050aedf5f227afbdaaaa0aae8a6cd74bfc766b8
Martin Corino <mcorino@m2c-software.nl>
2024-08-10 15:35:41 +0200

    minor cleanup

--- rakelib/lib/director/dc.rb
+++ rakelib/lib/director/dc.rb
@@ -16,11 +16,13 @@ module WXRuby3
 
       def setup
         super
-        _readDC = 'wxDC'
-        if Config.instance.wx_version >= '3.3.0'
-          spec.items << 'wxReadOnlyDC' << 'wxInfoDC'
-          _readDC = 'wxReadOnlyDC'
-        end
+        _readDC = if Config.instance.wx_version >= '3.3.0'
+                    spec.items.unshift 'wxReadOnlyDC' # prepend before wxDC
+                    spec.items << 'wxInfoDC'
+                    'wxReadOnlyDC'
+                  else
+                    'wxDC'
+                  end
         spec.items << 'wxFontMetrics'
         # it's not safe to track DC objects as these are often created on the stack in C++
         # before being passed to Ruby methods
@@ -64,30 +66,30 @@ module WXRuby3
               }
             return rb_result;
           }
-          __HEREDOC
+        __HEREDOC
         # for GetUserScale and GetLogicalScale
         spec.map_apply 'double * OUTPUT' => 'double *'
         spec.swig_import 'swig/classes/include/wxGDICommon.h'
         if Config.instance.wx_version >= '3.3.0'
           # add similar block-style creator as #draw_on methods
           spec.add_extend_code 'wxInfoDC', <<~__HEREDOC
-              static VALUE inform_on(wxWindow* win)
+            static VALUE inform_on(wxWindow* win)
+            {
+              if (!wxRuby_IsAppRunning()) 
+                rb_raise(rb_eRuntimeError, "A running Wx::App is required.");
+              if (!win)
+                rb_raise(rb_eRuntimeError, "A running valid Wx::Window is required for argument.");
+              VALUE rc = Qnil;
+              if (rb_block_given_p ())
               {
-                if (!wxRuby_IsAppRunning()) 
-                  rb_raise(rb_eRuntimeError, "A running Wx::App is required.");
-                if (!win)
-                  rb_raise(rb_eRuntimeError, "A running valid Wx::Window is required for argument.");
-                VALUE rc = Qnil;
-                if (rb_block_given_p ())
-                {
-                  wxInfoDC info_dc(win);
-                  wxReadOnlyDC* dc_ptr = &info_dc; // wxInfoDC::operator&() returns wxReadOnlyDC*
-                  VALUE rb_dc = SWIG_NewPointerObj(SWIG_as_voidptr(dc_ptr), SWIGTYPE_p_wxInfoDC, 0);
-                  rc = rb_yield(rb_dc);
-                }
-                return rc;
+                wxInfoDC info_dc(win);
+                wxReadOnlyDC* dc_ptr = &info_dc; // wxInfoDC::operator&() returns wxReadOnlyDC*
+                VALUE rb_dc = SWIG_NewPointerObj(SWIG_as_voidptr(dc_ptr), SWIGTYPE_p_wxInfoDC, 0);
+                rc = rb_yield(rb_dc);
               }
-            __HEREDOC
+              return rc;
+            }
+          __HEREDOC
         end
       end
     end # class DC
