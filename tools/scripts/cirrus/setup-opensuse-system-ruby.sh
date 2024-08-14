b9f4991bf8c147b3f77cb33991b4bd9979d2efab
Martin Corino <mcorino@m2c-software.nl>
2024-08-14 14:33:38 +0200

    fix testing with OLD system Ruby on OpenSuSE

--- tools/scripts/cirrus/setup-opensuse-system-ruby.sh
+++ tools/scripts/cirrus/setup-opensuse-system-ruby.sh
@@ -5,4 +5,6 @@ if [ "$1" == "remove" ]; then
   zypper install -y libyaml-devel libopenssl-devel
 else
   zypper install -y ruby ruby-devel ruby2.5-rubygem-bundler zlib-devel
+  # provide older minitest compatible with system Ruby (2.5)
+  gem install minitest -v 5.15.0
 fi
