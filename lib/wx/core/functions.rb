4a175096a16c9b8ed14071a39fa558b541bc4358
Martin Corino <mcorino@m2c-software.nl>
2024-09-28 17:08:15 +0200

    fix outdated about_box customizations

--- lib/wx/core/functions.rb
+++ lib/wx/core/functions.rb
@@ -13,20 +13,14 @@ module Wx
     # Allow this to be called with keyword parameters, and avoid
     # segfaults on OS X with bad params
     wx_about_box = self.instance_method(:about_box)
-    define_method(:about_box) do | info |
-      # If AboutDialogInfo has no version, it segfaults on OS X 10.5
-      no_version = ArgumentError.new("Must supply a version for AboutDialog")
+    define_method(:about_box) do | info, parent=nil |
       case info
       when Wx::AboutDialogInfo
-        unless info.has_version
-          Kernel.raise no_version 
-        end
-
         ab_info = info
       when Hash
         ab_info = Wx::AboutDialogInfo.new
         ab_info.name    = info[:name] || 'wxRuby application'
-        ab_info.version = info[:version] || Kernel.raise(no_version)
+        ab_info.version = info[:version] if info[:version]
         
         ab_info.description = info[:description] || ''
         ab_info.copyright   = info[:copyright] || ''
@@ -36,7 +30,7 @@ module Wx
         ab_info.artists     = info[:artists] || []
         ab_info.translators = info[:translators] || []
         if info.key?(:website)
-          ab_info.set_website(*info[:website])
+          ab_info.set_web_site(*info[:website])
         end
         if info.key?(:icon)
           ab_info.icon = info[:icon]
@@ -46,7 +40,7 @@ module Wx
         Kernel.raise ArgumentError,
                      "Can't use #{info.inspect} for AboutDialogInfo"
       end
-      wx_about_box.bind(self).call(ab_info)
+      wx_about_box.bind(self).call(ab_info, parent)
     end
   end
 end
