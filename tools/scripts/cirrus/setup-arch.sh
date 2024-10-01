a7e17edeb5403d1ef68811f8d8ee4fbfb010ce99
Martin Corino <mcorino@m2c-software.nl>
2024-10-01 11:33:54 +0200

    add package database refresh

--- tools/scripts/cirrus/setup-arch.sh
+++ tools/scripts/cirrus/setup-arch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+pacman -Sy --noconfirm
 pacman -q -S --noconfirm --needed which git make gcc autogen automake autoconf pkg-config libyaml
 if [ "$1" == "test" ]; then
   pacman -q -S --no-confirm --needed pkg-config gtk3 webkit2gtk gspell libunwind gstreamer curl libsecret libnotify libpng12
