ec64488c9c84543d76779eeabf0ab9cf49f0ff48
Martin Corino <mcorino@m2c-software.nl>
2024-10-06 13:45:39 +0200

    update and expand exception tests

---
+++ tests/lib/leaked_process_event_exception_test.rb
@@ -0,0 +1,33 @@
+# Copyright (c) 2023 M.J.N. Corino, The Netherlands
+#
+# This software is released under the MIT license.
+
+require_relative './wxapp_runner'
+
+class ProcessEventHandlingExceptionTests < Test::Unit::TestCase
+
+  class TestEvent < Wx::Event
+    EVT_TEST_EVENT = Wx::EvtHandler.register_class(self, nil, 'evt_test_event', 0)
+    def initialize(id=0)
+      super(EVT_TEST_EVENT, id)
+    end
+  end
+
+  class TestFrame < Wx::Frame
+
+    def initialize
+      super(nil, size: [300,300])
+
+      evt_test_event { |_evt| raise RuntimeError, 'Whatever' }
+    end
+
+  end
+
+  def test_process_event
+    win = TestFrame.new
+    win.process_event(TestEvent.new)
+    win.destroy
+    10.times { Wx.get_app.yield }
+  end
+
+end
