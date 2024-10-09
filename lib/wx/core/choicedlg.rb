bab442c51dd72e4b05a9d5bdaaf05ffbb61935b4
Martin Corino <mcorino@m2c-software.nl>
2024-10-09 15:50:26 +0200

    improve/correct selection dialog methods

--- lib/wx/core/choicedlg.rb
+++ lib/wx/core/choicedlg.rb
@@ -14,9 +14,8 @@ module Wx
                              choices,
                              parent = nil,
                              initial_selection: 0,
-                             x:  Wx::DEFAULT_COORD,
-                             y:  Wx::DEFAULT_COORD)
-    dialog = Wx::SingleChoiceDialog.new(parent, message, caption, choices, Wx::CHOICEDLG_STYLE, [x, y])
+                             pos:  Wx::DEFAULT_POSITION)
+    dialog = Wx::SingleChoiceDialog.new(parent, message, caption, choices, Wx::CHOICEDLG_STYLE, pos)
 
     dialog.selection = initial_selection
     return dialog.show_modal == Wx::ID_OK ? dialog.get_string_selection : ''
@@ -28,9 +27,8 @@ module Wx
                                    choices,
                                    parent = nil,
                                    initial_selection: 0,
-                                   x:  Wx::DEFAULT_COORD,
-                                   y:  Wx::DEFAULT_COORD)
-    dialog = Wx::SingleChoiceDialog.new(parent, message, caption, choices, Wx::CHOICEDLG_STYLE, [x, y])
+                                   pos:  Wx::DEFAULT_POSITION)
+    dialog = Wx::SingleChoiceDialog.new(parent, message, caption, choices, Wx::CHOICEDLG_STYLE, pos)
 
     dialog.selection = initial_selection
     return dialog.show_modal == Wx::ID_OK ? dialog.get_selection : -1
@@ -43,16 +41,15 @@ module Wx
                                 choices,
                                 parent = nil,
                                 initial_selections: [],
-                                x:  Wx::DEFAULT_COORD,
-                                y:  Wx::DEFAULT_COORD)
-    dialog = Wx::MultiChoiceDialog.new(parent, message, caption, choices, Wx::CHOICEDLG_STYLE, [x, y])
+                                pos:  Wx::DEFAULT_POSITION)
+    dialog = Wx::MultiChoiceDialog.new(parent, message, caption, choices, Wx::CHOICEDLG_STYLE, pos)
 
     # call this even if selections array is empty and this then (correctly)
     # deselects the first item which is selected by default
     dialog.selections = initial_selections
 
     if dialog.show_modal != Wx::ID_OK
-      return []
+      return nil
     end
 
     dialog.get_selections
