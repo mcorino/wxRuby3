e35931adb0819425df0dfca3e29ccac8124e44e2
Martin Corino <mcorino@m2c-software.nl>
2024-10-25 14:40:07 +0200

    add support for Wx::AUI::AuiSerializer and Wx::AUI::AuiDeserializer for wxw >= 3.3.0

--- lib/wx/aui/auimanager.rb
+++ lib/wx/aui/auimanager.rb
@@ -2,19 +2,66 @@
 #
 # This software is released under the MIT license.
 
-class Wx::AUI::AuiManager
-  def get_all_panes
-    ::Enumerator.new { |y| each_pane { |p| y << p } }
-  end
-  alias :all_panes :get_all_panes
+module Wx
+  module AUI
 
-  unless Wx::EvtHandler.event_type_for_name(:evt_aui_find_manager)
-    # missing from XML API refs
-    Wx::EvtHandler.register_event_type Wx::EvtHandler::EventType[
-      'evt_aui_find_manager', 0,
-      Wx::AUI::EVT_AUI_FIND_MANAGER,
-      Wx::AUI::AuiManagerEvent
-    ] if Wx::AUI.const_defined?(:EVT_AUI_FIND_MANAGER)
-  end
+    class AuiManager
 
+      wx_each_pane = instance_method(:each_pane)
+      define_method(:each_pane) do |&block|
+        if block
+          wx_each_pane.bind(self).call(&block)
+        else
+          ::Enumerator.new { |y| wx_each_pane.bind(self).call { |p| y << p } }
+        end
+      end
+
+      def get_all_panes
+        each_pane.to_a
+      end
+      alias :all_panes :get_all_panes
+
+      unless Wx::EvtHandler.event_type_for_name(:evt_aui_find_manager)
+        # missing from XML API refs
+        Wx::EvtHandler.register_event_type Wx::EvtHandler::EventType[
+                                             'evt_aui_find_manager', 0,
+                                             Wx::AUI::EVT_AUI_FIND_MANAGER,
+                                             Wx::AUI::AuiManagerEvent
+                                           ] if Wx::AUI.const_defined?(:EVT_AUI_FIND_MANAGER)
+      end
+    end
+
+    if WXWIDGETS_VERSION >= '3.3.0'
+
+      class AuiDockInfo
+
+        wx_each_pane = instance_method(:each_pane)
+        define_method(:each_pane) do |&block|
+          if block
+            wx_each_pane.bind(self).call(&block)
+          else
+            ::Enumerator.new { |y| wx_each_pane.bind(self).call { |p| y << p } }
+          end
+        end
+
+        def get_panes
+          each_pane.to_a
+        end
+        alias :panes :get_panes
+
+      end
+
+      class AuiDeserializer
+
+        wx_initialize = instance_method(:initialize)
+        define_method(:initialize) do |manager|
+          wx_initialize.bind(self).call(manager)
+          @manager = manager # prevent GC for lifetime of deserializer
+        end
+
+      end
+
+    end
+
+  end
 end
