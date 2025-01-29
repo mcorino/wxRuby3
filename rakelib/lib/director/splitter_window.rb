d2848d5c2eddd597c10eb4fbda5f2cbc3213a2b6
Martin Corino <mcorino@m2c-software.nl>
2025-01-29 10:42:36 +0100

    fix event constant naming mismatch

--- rakelib/lib/director/splitter_window.rb
+++ rakelib/lib/director/splitter_window.rb
@@ -20,9 +20,26 @@ module WXRuby3
         # is missing from the XML docs
         spec.extend_interface('wxSplitterWindow', 'virtual void OnInternalIdle()')
         super
+        # fix naming mismatch with #evt_splitter_dclick
+        spec.add_swig_code '%constant int EVT_SPLITTER_DCLICK = wxEVT_SPLITTER_DOUBLECLICKED;'
       end
+
+      def doc_generator
+        SplitterWindowDocGenerator.new(self)
+      end
+
     end # class SplitterWindow
 
   end # class Director
+
+  class SplitterWindowDocGenerator < DocGenerator
+
+    protected def gen_constants_doc(fdoc)
+      super
+      xref_table = package.all_modules.reduce(DocGenerator.constants_db) { |db, mod| db[mod] }
+      gen_constant_doc(fdoc, 'EVT_SPLITTER_DCLICK', xref_table['EVT_SPLITTER_DOUBLECLICKED'], 'wxRuby specific alias for Wx::EVT_SPLITTER_DOUBLECLICKED')
+    end
+
+  end
 
 end # module WXRuby3
