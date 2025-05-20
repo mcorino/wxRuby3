7e4f4317bdb0f4d3bd93e7954b642bad8d667c15
Martin Corino <mcorino@m2c-software.nl>
2025-05-20 12:22:50 +0200

    add wxWidgets version check helper methods

--- lib/wx/doc/core.rb
+++ lib/wx/doc/core.rb
@@ -12,6 +12,27 @@ module Wx
     # In case #wxrb_debug returns true #wxrb_trace_level= is also defined)
     # @return [Integer]
     attr_reader :wrb_trace_level
+
+    # Returns true if WXWIDGETS_VERSION >= ver
+    # @param [String,Array(Integer)] ver version string or integer array (1-3)
+    # @return [Boolean] true if WXWIDGETS_VERSION >= ver, false otherwise
+    def self.at_least_wxwidgets?(ver) end
+
+    # Returns true if WXWIDGETS_VERSION <= ver
+    # @param [String,Array(Integer)] ver version string or integer array (1-3)
+    # @return [Boolean] true if WXWIDGETS_VERSION <= ver, false otherwise
+    def self.up_to_wxwidgets?(ver) end
+
+    # Returns true if WXWIDGETS_VERSION < ver
+    # @param [String,Array(Integer)] ver version string or integer array (1-3)
+    # @return [Boolean] true if WXWIDGETS_VERSION < ver, false otherwise
+    def before_wxwidgets?(ver) end
+
+    # Returns true if WXWIDGETS_VERSION > ver
+    # @param [String,Array(Integer)] ver version string or integer array (1-3)
+    # @return [Boolean] true if WXWIDGETS_VERSION > ver, false otherwise
+    def after_wxwidgets?(ver) end
+
   end
 
 end
