ed55f956a236732bfa5c21ba07fa7cab046fb809
Martin Corino <martin@corino.nl>
2025-10-28 16:49:18 +0100

    add 'which' package

--- tools/scripts/cirrus/setup-opensuse.sh
+++ tools/scripts/cirrus/setup-opensuse.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-zypper install -y git xorg-x11-server-Xvfb xvfb-run xauth xorg-x11-fonts curl gcc make tar gzip xz zlib-devel libyaml-devel libopenssl-devel libffi-devel readline-devel libwebp-devel libexpat-devel
+zypper install -y git xorg-x11-server-Xvfb xvfb-run xauth xorg-x11-fonts which curl gcc make tar gzip xz zlib-devel libyaml-devel libopenssl-devel libffi-devel readline-devel libwebp-devel libexpat-devel
 if [ "$1" == "test" ]; then
   zypper install -y gtk3 webkit2gtk3-devel libGLU1 gspell libnotify4 libsecret curl
 fi
