79e52376556bfc1ed5783f94e1323c901e90b7b7
Martin Corino <martin@corino.nl>
2025-10-28 16:36:19 +0100

    update ArchLinux support

--- tools/scripts/cirrus/setup-arch.sh
+++ tools/scripts/cirrus/setup-arch.sh
@@ -3,5 +3,5 @@
 pacman -Sy --noconfirm
 pacman -q -S --noconfirm --needed which git make gcc autogen automake autoconf pkg-config libyaml xorg-server-xvfb xorg-fonts-75dpi
 if [ "$1" == "test" ]; then
-  pacman -q -S --no-confirm --needed pkg-config gtk3 webkit2gtk gspell libunwind gstreamer curl libsecret libnotify libpng12
+  pacman -q -S --no-confirm --needed pkg-config gtk3 webkit2gtk gspell libunwind gstreamer curl libsecret libnotify libpng
 fi
