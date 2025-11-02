53b3a5c1093e1eedc4cb953c512a3532fe443b9f
Martin Corino <martin@corino.nl>
2025-11-02 11:55:14 +0100

    adding/updating Manjaro/Arch linux CI support

--- tools/scripts/cirrus/setup-arch.sh
+++ tools/scripts/cirrus/setup-arch.sh
@@ -3,5 +3,5 @@
 pacman -Sy --noconfirm
 pacman -q -S --noconfirm --needed which git make gcc autogen automake autoconf pkg-config libyaml xorg-server-xvfb xorg-fonts-75dpi
 if [ "$1" == "test" ]; then
-  pacman -q -S --noconfirm --needed pkg-config gtk3 webkit2gtk-4.1 gdk-pixbuf2 adwaita-icon-theme gnome-keyring gspell libvoikko hspell nuspell libunwind gstreamer curl libsecret libnotify libpng
+  pacman -q -S --noconfirm --needed pkg-config gtk3 webkit2gtk-4.1 gdk-pixbuf2 librsvg adwaita-icon-theme gnome-keyring gspell libvoikko hspell nuspell libunwind gstreamer curl libsecret libnotify libpng
 fi
--- tools/scripts/cirrus/setup-manjaro.sh
+++ tools/scripts/cirrus/setup-manjaro.sh
@@ -5,5 +5,5 @@ pacman -q -S --noconfirm --needed pamac-cli -d libxml2 libxml2-legacy
 
 pamac install --no-confirm glibc which git make gcc autogen automake autoconf pkgconf libyaml xorg-server-xvfb xorg-fonts-75dpi
 if [ "$1" == "test" ]; then
-  pamac install --no-confirm gtk3 webkit2gtk-4.1 gdk-pixbuf2 adwaita-icon-theme gnome-keyring gspell libvoikko hspell nuspell libunwind gstreamer curl libsecret libnotify libpng
+  pamac install --no-confirm gtk3 webkit2gtk-4.1 gdk-pixbuf2 librsvg adwaita-icon-theme gnome-keyring gspell libvoikko hspell nuspell libunwind gstreamer curl libsecret libnotify libpng
 fi
