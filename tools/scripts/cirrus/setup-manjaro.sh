17ba3ccaedfc6f95cfb62d96273f6eb7d877d223
Martin Corino <martin@corino.nl>
2025-11-02 11:49:38 +0100

    adding Manjaro linux CI support

--- tools/scripts/cirrus/setup-manjaro.sh
+++ tools/scripts/cirrus/setup-manjaro.sh
@@ -3,7 +3,7 @@
 pacman -Syyu
 pacman -q -S --noconfirm --needed pamac-cli -d libxml2 libxml2-legacy
 
-pamac install --no-confirm which git make gcc autogen automake autoconf pkgconf libyaml xorg-server-xvfb xorg-fonts-75dpi
+pamac install --no-confirm glibc which git make gcc autogen automake autoconf pkgconf libyaml xorg-server-xvfb xorg-fonts-75dpi
 if [ "$1" == "test" ]; then
   pamac install --no-confirm gtk3 webkit2gtk-4.1 gdk-pixbuf2 adwaita-icon-theme gnome-keyring gspell libvoikko hspell nuspell libunwind gstreamer curl libsecret libnotify libpng
 fi
