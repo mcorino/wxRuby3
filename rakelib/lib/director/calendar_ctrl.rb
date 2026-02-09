aa450600339fa55c20d7de8c21520e8d30b71de1
Martin Corino <martin@corino.nl>
2026-02-09 12:56:09 +0100

    add wxGenericCalendarCtrl support

--- rakelib/lib/director/calendar_ctrl.rb
+++ rakelib/lib/director/calendar_ctrl.rb
@@ -77,22 +77,80 @@ module WXRuby3
         spec.disown 'wxCalendarDateAttr* attr'
 
         spec.add_extend_code 'wxCalendarCtrl', <<~__HEREDOC
-        VALUE hit_test(wxPoint& pos)
-        {
-          wxDateTime hit_date;
-          wxDateTime::WeekDay hit_wkday;
-          wxCalendarHitTestResult hit = $self->HitTest(pos, &hit_date, &hit_wkday);
-          if ( hit == wxCAL_HITTEST_HEADER ) {
-            return(INT2NUM(hit_wkday));
+          VALUE hit_test(wxPoint& pos)
+          {
+            wxDateTime hit_date;
+            wxDateTime::WeekDay hit_wkday;
+            wxCalendarHitTestResult hit = $self->HitTest(pos, &hit_date, &hit_wkday);
+            if ( hit == wxCAL_HITTEST_HEADER ) {
+              return(INT2NUM(hit_wkday));
+            }
+            else if ( hit == wxCAL_HITTEST_DAY ) {
+              return(wxRuby_wxDateTimeToRuby(hit_date));
+            }
+            // Assume that hit == wxCAL_HITTEST_NOWHERE
+            return Qnil;
           }
-          else if ( hit == wxCAL_HITTEST_DAY ) {
-            return(wxRuby_wxDateTimeToRuby(hit_date));
-          }
-          // Assume that hit == wxCAL_HITTEST_NOWHERE
-          return Qnil;
-        }
-        __HEREDOC
+          __HEREDOC
         spec.do_not_generate(:variables, :enums, :defines, :functions)
+      end
+
+      def process(gendoc: false)
+        defmod = super
+        # create definition for wxGenericCalendarCtrl except for WXOSX where
+        # wxCalendarCtrl is simply an alias for that
+        unless Config.instance.features_set?('WXOSX')
+          spec.include 'wx/generic/calctrlg.h'
+          def_calctrl = defmod.find_item('wxCalendarCtrl')
+          # create a definition for 'wxGenericStaticBitmap' which is not documented
+          def_gencalctrl = def_calctrl.dup
+          def_gencalctrl.name = 'wxGenericCalendarCtrl'
+          def_gencalctrl.brief_doc = nil
+          def_gencalctrl.detailed_doc = nil
+          def_gencalctrl.items = def_gencalctrl.items.collect { |itm| itm.dup }
+          def_gencalctrl.items.each do |itm|
+            if itm.is_a?(Extractor::MethodDef)
+              itm.overloads = itm.overloads.collect { |ovl| ovl.dup }
+              itm.all.each do |ovl|
+                ovl.name = 'wxGenericCalendarCtrl' if ovl.is_ctor
+                ovl.class_name = 'wxGenericCalendarCtrl'
+                ovl.update_attributes(klass: def_gencalctrl)
+              end
+            end
+          end
+          defmod.items << def_gencalctrl
+          # as we already called super before adding wxGenericCalendarCtrl the no_proxy settings from the
+          # base Window director are missing; just copy all those set for wxStaticBitmap
+          list = spec.no_proxies.select { |name| name.start_with?('wxCalendarCtrl::') }
+          spec.no_proxy(*list.collect { |name| name.sub(/\AwxCalendarCtrl::/, 'wxGenericCalendarCtrl::')})
+          # other specs as for wxCalendarCtrl
+          # handled; can be suppressed
+          spec.suppress_warning(473,
+                                'wxGenericCalendarCtrl::GetAttr',
+                                'wxGenericCalendarCtrl::GetHeaderColourBg',
+                                'wxGenericCalendarCtrl::GetHeaderColourFg',
+                                'wxGenericCalendarCtrl::GetHighlightColourBg',
+                                'wxGenericCalendarCtrl::GetHighlightColourFg',
+                                'wxGenericCalendarCtrl::GetHolidayColourBg',
+                                'wxGenericCalendarCtrl::GetHolidayColourFg')
+          spec.add_extend_code 'wxGenericCalendarCtrl', <<~__HEREDOC
+            VALUE hit_test(wxPoint& pos)
+            {
+              wxDateTime hit_date;
+              wxDateTime::WeekDay hit_wkday;
+              wxCalendarHitTestResult hit = $self->HitTest(pos, &hit_date, &hit_wkday);
+              if ( hit == wxCAL_HITTEST_HEADER ) {
+                return(INT2NUM(hit_wkday));
+              }
+              else if ( hit == wxCAL_HITTEST_DAY ) {
+                return(wxRuby_wxDateTimeToRuby(hit_date));
+              }
+              // Assume that hit == wxCAL_HITTEST_NOWHERE
+              return Qnil;
+            }
+            __HEREDOC
+        end
+        defmod
       end
     end # class CalendarCtrl
 
