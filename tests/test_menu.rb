4b3f648838516a8cdb31d9c4d79905ceec452cf9
Martin Corino <martin@corino.nl>
2026-03-04 16:46:41 +0100

    extend tests

--- tests/test_menu.rb
+++ tests/test_menu.rb
@@ -4,7 +4,94 @@
 
 require_relative './lib/wxframe_runner'
 
-class MenuTests < WxRuby::Test::GUITests
+# class MenuTests < WxRuby::Test::GUITests
+#
+#   def setup
+#     super
+#     @menu = Wx::Menu.new
+#     @menu.append(Wx::ID_HIGHEST+1, 'Item 1', 'Test menuitem 1')
+#     @menu.append(Wx::ID_HIGHEST+2, 'Item 2', 'Test menuitem 2')
+#     @menu.append_separator
+#     @menu.append_check_item(Wx::ID_HIGHEST+3, 'Check 3', 'Test menuitem 3')
+#     @menu.append_separator
+#     @menu.append_radio_item(Wx::ID_HIGHEST+4, 'Radio 4', 'Test menuitem 4')
+#     @menu.append_radio_item(Wx::ID_HIGHEST+5, 'Radio 5', 'Test menuitem 5')
+#     @menu.append_radio_item(Wx::ID_HIGHEST+6, 'Radio 6', 'Test menuitem 6')
+#     frame_win.menu_bar = Wx::MenuBar.new
+#     frame_win.menu_bar.append(@menu, "&Test")
+#     GC.start
+#   end
+#
+#   def teardown
+#     frame_win.menu_bar = nil
+#     super
+#     GC.start
+#   end
+#
+#   attr_reader :menu
+#
+#   def test_basic
+#     assert_equal(8, menu.get_menu_item_count)
+#     check, pos = menu.find_child_item(Wx::ID_HIGHEST+3)
+#     assert_not_nil(check)
+#     assert_equal(3, pos)
+#     assert_equal('Check 3', check.item_label)
+#     assert_false(check.checked?)
+#     check.check
+#     assert_true(check.checked?)
+#     radio, pos = menu.find_child_item(Wx::ID_HIGHEST+5)
+#     assert_not_nil(radio)
+#     assert_equal(6, pos)
+#     assert_equal('Radio 5', radio.item_label)
+#     assert_false(radio.checked?)
+#     radio.check
+#     assert_true(radio.checked?)
+#     radio, pos = menu.find_child_item(Wx::ID_HIGHEST+4)
+#     assert_false(radio.checked?)
+#     radio, pos = menu.find_child_item(Wx::ID_HIGHEST+6)
+#     assert_false(radio.checked?)
+#     radio.check
+#     assert_true(radio.checked?)
+#     radio, pos = menu.find_child_item(Wx::ID_HIGHEST+5)
+#     assert_false(radio.checked?)
+#   end
+#
+#   def test_enumerate
+#     radio, pos = menu.find_child_item(Wx::ID_HIGHEST+4)
+#     assert_true(radio.checked?) # first by default checked
+#     menu.each_item do |mi|
+#       mi.check if mi.id == (Wx::ID_HIGHEST+5)
+#     end
+#     radio, pos = menu.find_child_item(Wx::ID_HIGHEST+4)
+#     assert_false(radio.checked?)
+#     radio = menu.each_item.detect { |mi| mi.id == (Wx::ID_HIGHEST+5) }
+#     assert_true(radio.checked?)
+#   end
+#
+#   def test_accelerators
+#     if Wx.has_feature?(:USE_ACCEL)
+#       10.times do
+#         accel_keys = { "Z" => Wx::ID_UNDO,
+#                        "Y" => Wx::ID_REDO,
+#                        "C" => Wx::ID_COPY,
+#                        "X" => Wx::ID_CUT,
+#                        "V" => Wx::ID_PASTE }
+#         accel_table = accel_keys.keys.map do | key |
+#           [ Wx::MOD_CMD, key, accel_keys[key] ]
+#         end
+#
+#         accel_table = Wx::AcceleratorTable[ *accel_table ]
+#         frame_win.accelerator_table = accel_table
+#         assert_true(accel_table.ok?)
+#         assert_true(frame_win.accelerator_table.ok?)
+#         GC.start
+#       end
+#     end
+#   end
+#
+# end
+
+class UnattachedMenuTests < WxRuby::Test::GUITests
 
   def setup
     super
@@ -17,74 +104,37 @@ class MenuTests < WxRuby::Test::GUITests
     @menu.append_radio_item(Wx::ID_HIGHEST+4, 'Radio 4', 'Test menuitem 4')
     @menu.append_radio_item(Wx::ID_HIGHEST+5, 'Radio 5', 'Test menuitem 5')
     @menu.append_radio_item(Wx::ID_HIGHEST+6, 'Radio 6', 'Test menuitem 6')
-    frame_win.menu_bar = Wx::MenuBar.new
-    frame_win.menu_bar.append(@menu, "&Test")
+    @menu_bar = Wx::MenuBar.new
+    @menu2 = Wx::Menu.new
+    @menu2.append(Wx::ID_HIGHEST+1, 'Item 1', 'Test menuitem 1')
+    @menu2.append(Wx::ID_HIGHEST+2, 'Item 2', 'Test menuitem 2')
+    @menu2.append_separator
+    @menu2.append_check_item(Wx::ID_HIGHEST+3, 'Check 3', 'Test menuitem 3')
+    @menu2.append_separator
+    @menu2.append_radio_item(Wx::ID_HIGHEST+4, 'Radio 4', 'Test menuitem 4')
+    @menu2.append_radio_item(Wx::ID_HIGHEST+5, 'Radio 5', 'Test menuitem 5')
+    @menu2.append_radio_item(Wx::ID_HIGHEST+6, 'Radio 6', 'Test menuitem 6')
+    @menu_bar2 = Wx::MenuBar.new
+    GC.start
   end
 
   def teardown
-    frame_win.menu_bar = nil
     super
+    GC.start
   end
 
-  attr_reader :menu
-
-  def test_basic
-    assert_equal(8, menu.get_menu_item_count)
-    check, pos = menu.find_child_item(Wx::ID_HIGHEST+3)
-    assert_not_nil(check)
-    assert_equal(3, pos)
-    assert_equal('Check 3', check.item_label)
-    assert_false(check.checked?)
-    check.check
-    assert_true(check.checked?)
-    radio, pos = menu.find_child_item(Wx::ID_HIGHEST+5)
-    assert_not_nil(radio)
-    assert_equal(6, pos)
-    assert_equal('Radio 5', radio.item_label)
-    assert_false(radio.checked?)
-    radio.check
-    assert_true(radio.checked?)
-    radio, pos = menu.find_child_item(Wx::ID_HIGHEST+4)
-    assert_false(radio.checked?)
-    radio, pos = menu.find_child_item(Wx::ID_HIGHEST+6)
-    assert_false(radio.checked?)
-    radio.check
-    assert_true(radio.checked?)
-    radio, pos = menu.find_child_item(Wx::ID_HIGHEST+5)
-    assert_false(radio.checked?)
-  end
-
-  def test_enumerate
-    radio, pos = menu.find_child_item(Wx::ID_HIGHEST+4)
-    assert_true(radio.checked?) # first by default checked
-    menu.each_item do |mi|
-      mi.check if mi.id == (Wx::ID_HIGHEST+5)
-    end
-    radio, pos = menu.find_child_item(Wx::ID_HIGHEST+4)
-    assert_false(radio.checked?)
-    radio = menu.each_item.detect { |mi| mi.id == (Wx::ID_HIGHEST+5) }
-    assert_true(radio.checked?)
-  end
-
-  def test_accelerators
-    if Wx.has_feature?(:USE_ACCEL)
-      10.times do
-        accel_keys = { "Z" => Wx::ID_UNDO,
-                       "Y" => Wx::ID_REDO,
-                       "C" => Wx::ID_COPY,
-                       "X" => Wx::ID_CUT,
-                       "V" => Wx::ID_PASTE }
-        accel_table = accel_keys.keys.map do | key |
-          [ Wx::MOD_CMD, key, accel_keys[key] ]
-        end
-
-        accel_table = Wx::AcceleratorTable[ *accel_table ]
-        frame_win.accelerator_table = accel_table
-        assert_true(accel_table.ok?)
-        assert_true(frame_win.accelerator_table.ok?)
-        GC.start
-      end
-    end
+  def test_menu_bar
+    yield_for_a_while(200)
+    GC.start
+    @menu_bar.append(@menu, "&Test")
+    yield_for_a_while(200)
+    GC.start
+    frame_win.menu_bar = @menu_bar
+    yield_for_a_while(200)
+    GC.start
+    frame_win.menu_bar = nil
+    yield_for_a_while(200)
+    GC.start
   end
 
 end
