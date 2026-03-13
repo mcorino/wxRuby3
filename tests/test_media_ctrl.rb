d53a63a50048573156bb6814f9c6df4b3180213c
Martin Corino <martin@corino.nl>
2026-03-13 11:47:00 +0100

    exclude incompatible test for MacOS

--- tests/test_media_ctrl.rb
+++ tests/test_media_ctrl.rb
@@ -27,19 +27,18 @@ class MediaCtrlTests < WxRuby::Test::GUITests
   attr_accessor :media_loaded
 
   def test_uri
-    # just checking if Ruby URI is properly mapped
-    # it doesn't seem the mediactrl returns a very consistent result whether the resource is not a media file
-    # or if it is a non-existing file so only check whether it is a boolean return.
     self.media_loaded = false
     uri = URI("file://#{File.join(__dir__, 'media/beep_lo.wav')}")
-    assert_boolean(media.load(uri))
+    assert_true(media.load(uri))
     yield_and_wait_for_test(5000) { self.media_loaded }
     assert_true(self.media_loaded)
-    self.media_loaded = false
-    uri = URI("file://#{File.join(__dir__, 'art/test_art/image/wxruby.png')}")
-    assert_boolean(media.load(uri))
-    yield_and_wait_for_test(5000) { self.media_loaded }
-    assert_true(self.media_loaded)
+    unless is_macos?
+      self.media_loaded = false
+      uri = URI("file://#{File.join(__dir__, 'art/test_art/image/wxruby.png')}")
+      assert_true(media.load(uri))
+      yield_and_wait_for_test(5000) { self.media_loaded }
+      assert_true(self.media_loaded)
+    end
   end
 
 end
