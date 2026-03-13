366b79c0bdd521078092118139a46346dfd6eeb3
Martin Corino <martin@corino.nl>
2026-03-13 10:07:29 +0100

    use new yield and wait to wait for load events

--- tests/test_media_ctrl.rb
+++ tests/test_media_ctrl.rb
@@ -13,7 +13,9 @@ class MediaCtrlTests < WxRuby::Test::GUITests
 
   def setup
     super
+    @media_loaded = false
     @media = Wx::MediaCtrl.new(frame_win)
+    frame_win.evt_media_loaded(@media) {|_| @media_loaded = true }
   end
 
   def teardown
@@ -22,17 +24,22 @@ class MediaCtrlTests < WxRuby::Test::GUITests
   end
 
   attr_reader :media
+  attr_accessor :media_loaded
 
   def test_uri
     # just checking if Ruby URI is properly mapped
     # it doesn't seem the mediactrl returns a very consistent result whether the resource is not a media file
     # or if it is a non-existing file so only check whether it is a boolean return.
+    self.media_loaded = false
     uri = URI("file://#{File.join(__dir__, 'media/beep_lo.wav')}")
     assert_boolean(media.load(uri))
-    yield_for_a_while(100)
+    yield_and_wait_for_test(1000) { self.media_loaded }
+    assert_true(self.media_loaded)
+    self.media_loaded = false
     uri = URI("file://#{File.join(__dir__, 'art/test_art/image/wxruby.png')}")
     assert_boolean(media.load(uri))
-    yield_for_a_while(100)
+    yield_and_wait_for_test(1000) { self.media_loaded }
+    assert_true(self.media_loaded)
   end
 
 end
