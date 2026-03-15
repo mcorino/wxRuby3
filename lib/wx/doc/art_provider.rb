29657183c5d0bc25b3f733f2e93b25dae2694480
Martin Corino <martin@corino.nl>
2026-03-15 10:41:36 +0100

    update for missing art ids

---
+++ lib/wx/doc/art_provider.rb
@@ -0,0 +1,17 @@
+# :stopdoc:
+# Copyright (c) 2023 M.J.N. Corino, The Netherlands
+#
+# This software is released under the MIT license.
+
+
+# This module provides and standardizes support for locating application art files.
+#
+# :startdoc:
+
+
+module Wx
+
+  ART_STOP = "wxART_STOP"
+  ART_REFRESH = "wxART_REFRESH"
+
+end
