6960b04534cb57f20255ab310bc1896de2528024
Martin Corino <martin@corino.nl>
2026-05-19 20:02:47 +0200

    reorder gem install switch

--- tools/scripts/docker/test-wxruby3-release.sh
+++ tools/scripts/docker/test-wxruby3-release.sh
@@ -18,7 +18,7 @@ echo "Testing wxRuby3 v$WXRUBY_RELEASE"
 
 if [ "$_test" == "1" ]; then
 
-  gem install ./$(echo wxruby3*.gem) -- package=`pwd`/$(echo wxruby3*.pkg) --no-format-executable && xvfb-run -a -s '-screen 0 1600x1200x24' wxruby test --exclude=test_intl --exclude=test_media_ctrl
+  gem install ./$(echo wxruby3*.gem) --no-format-executable -- package=`pwd`/$(echo wxruby3*.pkg) && xvfb-run -a -s '-screen 0 1600x1200x24' wxruby test --exclude=test_intl --exclude=test_media_ctrl
 
 else
   gem sources --add "https://mcorino:$GITHUB_TOKEN@rubygems.pkg.github.com/mcorino/"
