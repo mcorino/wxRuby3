2f4ae33be1954395a237332f2c247027532ec9a5
Martin Corino <martin@corino.nl>
2026-07-10 08:24:54 +0200

    correct package name

--- tools/scripts/docker/setup-opensuse-system-ruby.sh
+++ tools/scripts/docker/setup-opensuse-system-ruby.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "remove" ]; then
-  zypper remove -y ruby ruby-devel ruby-rubygem-bundler
+  zypper remove -y ruby ruby-devel rubygem-bundler
   #zypper install -y libyaml-devel libopenssl-devel
 else
-  zypper install -y ruby ruby-devel ruby-rubygem-bundler
+  zypper install -y ruby ruby-devel rubygem-bundler
   #zlib-devel
   ## provide older gems compatible with system Ruby (2.5)
   #gem install minitest -v 5.15.0
