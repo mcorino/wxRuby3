6280780204d0074b84300347f0b21981b4dd0da7
Martin Corino <martin@corino.nl>
2026-07-11 10:20:55 +0200

    fix linuxmint builds

---
+++ tools/scripts/docker/setup-linuxmint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt full-upgrade -y
+apt-get install -y git make gcc gpg xvfb xfonts-75dpi curl procps bzip2
+if [ "$1" == "test" ]; then
+  source /etc/os-release
+  case $VERSION_ID in
+    24.04|26.04)
+      apt-get install -y 'libgtk-3-[0-9]+' 'libwebkit2gtk-4.1-[0-9]+' 'libgspell-1-[0-9]+' libnotify4 'libsecret-1-[0-9]+' curl
+      ;;
+    *)
+      apt-get install -y 'libgtk-3-[0-9]+' 'libwebkit2gtk-4.0-[0-9]+' 'libgspell-1-[0-9]+' libnotify4 'libsecret-1-[0-9]+' curl
+      ;;
+  esac
+fi
